53d1a22e9e0d5f203b15fef8b88c9270898385f7
Sriram Krishnan <skrishnan@netflix.com>
2013-06-23 19:51:44 -0700

    Changing s3:// to hdfs:// so users can use in local mode easier

--- genie-web/src/test/python/utils/populateSampleConfigs.py
+++ genie-web/src/test/python/utils/populateSampleConfigs.py
@@ -31,7 +31,7 @@ def populateTestHive():
       <hiveConfig>
         <name>testhive</name>
         <type>TEST</type>
-        <s3HiveSiteXml>s3://BUCKET/PATH/TO/TEST-HIVE-SITE.XML</s3HiveSiteXml>
+        <s3HiveSiteXml>file://BUCKET/PATH/TO/TEST-HIVE-SITE.XML</s3HiveSiteXml>
         <user>testuser</user>
         <status>ACTIVE</status>
         <hiveVersion>0.8.1</hiveVersion>
@@ -47,7 +47,7 @@ def populateProdHive():
       <hiveConfig>
         <name>prodhive</name>
         <type>PROD</type>
-        <s3HiveSiteXml>s3://BUCKET/PATH/TO/PROD-HIVE-SITE.XML</s3HiveSiteXml>
+        <s3HiveSiteXml>file://BUCKET/PATH/TO/PROD-HIVE-SITE.XML</s3HiveSiteXml>
         <user>testuser</user>
         <status>ACTIVE</status>
         <hiveVersion>0.8.1</hiveVersion>
@@ -63,7 +63,7 @@ def populateTestPig():
       <pigConfig>
         <name>testpig</name>
         <type>TEST</type>
-        <s3PigProperties>s3://BUCKET/PATH/TO/TEST-PIG.PROPERTIES</s3PigProperties>
+        <s3PigProperties>file://BUCKET/PATH/TO/TEST-PIG.PROPERTIES</s3PigProperties>
         <user>testuser</user>
         <status>ACTIVE</status>
         <pigVersion>0.9.2</pigVersion>
@@ -79,7 +79,7 @@ def populateProdPig():
       <pigConfig>
         <name>prodpig</name>
         <type>PROD</type>
-        <s3PigProperties>s3://BUCKET/PATH/TO/PROD-PIG.PROPERTIES</s3PigProperties>
+        <s3PigProperties>file://BUCKET/PATH/TO/PROD-PIG.PROPERTIES</s3PigProperties>
         <user>testuser</user>
         <status>ACTIVE</status>
         <pigVersion>0.9.2</pigVersion>
@@ -99,9 +99,9 @@ def populateCluster():
         <adHoc>true</adHoc>
         <bonus>true</bonus>
         <sla>true</sla>
-        <s3MapredSiteXml>s3://PATH/TO/MAPRED-SITE.XML</s3MapredSiteXml>
-        <s3CoreSiteXml>s3://PATH/TO/CORE-SITE.XML</s3CoreSiteXml>
-        <s3HdfsSiteXml>s3://PATH/TO/HDFS-SITE.XML</s3HdfsSiteXml>
+        <s3MapredSiteXml>file://PATH/TO/MAPRED-SITE.XML</s3MapredSiteXml>
+        <s3CoreSiteXml>file://PATH/TO/CORE-SITE.XML</s3CoreSiteXml>
+        <s3HdfsSiteXml>file://PATH/TO/HDFS-SITE.XML</s3HdfsSiteXml>
         <user>testuser</user>
         <testHiveConfigId>hiveconf-testhive-sample</testHiveConfigId>
         <prodHiveConfigId>hiveconf-prodhive-sample</prodHiveConfigId>
