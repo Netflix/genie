5d4371f18f6ad51163c762accb2eeba79c0a80ce
Amit Sharma <amitsharma1708@gmail.com>
2014-04-17 12:51:06 -0700

    Three tests to submit jobs to a hadoop2 cluster.

---
+++ genie-web/src/test/python/jobs/pig2JobTest.py
@@ -0,0 +1,75 @@
+##
+#
+#  Copyright 2013 Netflix, Inc.
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+#
+##
+
+import sys
+sys.path.append('../utils')
+
+import time
+import eureka
+import jobs
+
+import os
+
+# the S3 prefix where the tests are located
+GENIE_TEST_PREFIX = os.getenv("GENIE_TEST_PREFIX")
+
+# get the serviceUrl from the eureka client
+serviceUrl = eureka.EurekaClient().getServiceBaseUrl() + '/genie/v0/jobs'
+
+def testJsonSubmitjob():
+    print "Running testJsonSubmitjob "
+    payload = '''
+    {
+        "jobInfo":
+        {
+            "jobName": "PIG-JOB-TEST", 
+            "userName" : "amsharma", 
+            "groupName" : "hadoop", 
+            "userAgent" : "laptop",
+            "jobType": "pig", 
+            "configuration": "prod", 
+            "schedule": "adHoc",
+            "clusterName": "h24query",
+            "cmdArgs":" -f pig2.q", 
+            "fileDependencies":"''' + GENIE_TEST_PREFIX + '''/pig2.q"
+        }
+    }
+    '''
+    print payload
+    print "\n"
+    return jobs.submitJob(serviceUrl, payload)
+
+# driver method for all tests
+if __name__ == "__main__":    
+    print "Running unit tests:\n"
+    jobID = testJsonSubmitjob()
+    print "\n"
+    
+    while True:
+       print jobs.getJobInfo(serviceUrl, jobID)
+       print "\n"
+       status = jobs.getJobStatus(serviceUrl, jobID)
+       print status
+       print "\n"
+
+       if (status != 'RUNNING') and (status != 'INIT'):
+           print "Final status: ", status
+           print "Job has terminated - exiting"
+           break
+              
+       time.sleep(5)
