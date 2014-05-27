84e5872f1ee97366042d39316dbb514e3ad40791
Amit Sharma <amitsharma1708@gmail.com>
2014-05-27 15:21:33 -0700

    Three scripts to add sample configs and reverting persistence.xml to use derby.

---
+++ genie-web/src/test/python/utils/registerCluster.py
@@ -0,0 +1,92 @@
+##
+#
+#  Copyright 2013 Netflix, Inc.
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+#
+##
+
+import sys
+sys.path.append('../utils')
+
+import time
+import eureka
+import json
+import os
+import uuid
+import restclient
+
+# get the serviceUrl from the eureka client
+serviceUrl = eureka.EurekaClient().getServiceBaseUrl() + '/genie/v1/config/cluster'
+
+def addClusterConfigCprod1():
+    print "Registering Cprod1"
+    ID="bdp_prod_20140101"
+    
+    tags = json.dumps(['sla','cprod1','bdp_prod_20140101'])
+    config = json.dumps(['s3://netflix-dataoven-prod/genie/cluster/bigdataplatform_prod_20140415/hdfs-site.xml','s3://netflix-dataoven-prod/genie/cluster/bigdataplatform_prod_20140415/core-site.xml','s3://netflix-dataoven-prod/genie/cluster/bigdataplatform_prod_20140415/mapred-site.xml'])
+    cmds = json.dumps(['prodhive11_mr1'])
+    
+    payload = '''
+    {
+        "clusterConfig":
+        {
+            "id":"''' + ID +'''",
+            "name": "cprod1", 
+            "status" : "UP",
+            "user" : "amsharma", 
+            "groupName" : "hadoop", 
+            "version" : "1.0.3",
+            "configs": ''' + config + ''',
+            "tags": ''' + tags + ''',
+            "cmdIds": ''' + cmds + ''' 
+        }
+    }
+    '''
+    print payload
+    print "\n"
+    print restclient.post(serviceUrl=serviceUrl, payload=payload, contentType='application/json')
+
+
+def addClusterConfigCquery1():
+    print "Registering cquery1"
+    ID="bdp_query_20140101"
+    tags = json.dumps(['adhoc'])
+    config = json.dumps(['s3://netflix-dataoven-prod/genie/cluster/bigdataplatform_query_20140518/mapred-site.xml','s3://netflix-dataoven-prod/genie/cluster/bigdataplatform_query_20140518/core-site.xml','s3://netflix-dataoven-prod/genie/cluster/bigdataplatform_query_20140518/hdfs-site.xml'])
+    cmds = json.dumps(['prodhive11_mr1'])
+    payload = '''
+    {
+        "clusterConfig":
+        {
+            "id":"''' + ID +'''",
+            "name": "cquery1", 
+            "status" : "UP",
+            "user" : "amsharma", 
+            "groupName" : "hadoop", 
+            "version" : "1.0.3",
+            "configs": ''' + config + ''',
+            "tags": ''' + tags + ''',
+            "cmdIds": ''' + cmds + ''' 
+        }
+    }
+    '''
+    print payload
+    print "\n"
+    print restclient.post(serviceUrl=serviceUrl, payload=payload, contentType='application/json')
+
+# driver method for all tests                
+if __name__ == "__main__":
+   print "Registering Clusters:\n"
+   print "\n"
+   addClusterConfigCprod1()
+   addClusterConfigCquery1()
