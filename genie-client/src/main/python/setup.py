6345bf191d60b482f988aeaff7a50ac4b2da0669
Tom Gianos <tgianos@netflix.com>
2014-08-21 15:26:24 -0700

    Refactoring the python client code to reformat it for the generated packages

---
+++ genie-client/src/main/python/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='genie-client',
+    version='0.1.2',
+    author='Netflix Inc.',
+    author_email='BigDataPlatform@netflix.com',
+    packages=['genie2', 'genie2.client', 'genie2.exception', 'genie2.model'],
+    scripts=[],
+    url='http://netflix.github.io/genie/',
+    license='LICENSE.txt',
+    description='Genie Python client api.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "python-dateutil >= 2.2",
+    ]
+)
