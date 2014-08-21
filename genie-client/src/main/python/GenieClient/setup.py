1b7d303691fa18a5ca51d9a244d216eee6564ab9
Amit Sharma <amitsharma1708@gmail.com>
2014-08-21 14:43:39 -0700

    Updated version.

--- genie-client/src/main/python/GenieClient/setup.py
+++ genie-client/src/main/python/GenieClient/setup.py
@@ -1,12 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='GenieClient',
-    version='0.1.0',
+    version='0.1.1',
     author='Neflix Inc.',
     author_email='bigdataplatform@netflix.com',
     packages=['genie_client', 'genie_client.test','genie_client.apis','genie_client.models','genie_client.exception'],
-    scripts=['bin/foo.py','bin/bar.py'],
     url='http://pypi.python.org/pypi/TowelStuff/',
     license='LICENSE.txt',
     description='Genie Python client api.',
