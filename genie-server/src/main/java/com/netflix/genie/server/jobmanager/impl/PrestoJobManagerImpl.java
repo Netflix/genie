b9824e81fb97a3c227b63b5cb200595a48394d84
Tom Gianos <tgianos@netflix.com>
2014-09-15 11:19:57 -0700

    Making presto domain come from properties

--- genie-server/src/main/java/com/netflix/genie/server/jobmanager/impl/PrestoJobManagerImpl.java
+++ genie-server/src/main/java/com/netflix/genie/server/jobmanager/impl/PrestoJobManagerImpl.java
@@ -46,6 +46,8 @@ import java.util.Map;
 public class PrestoJobManagerImpl extends JobManagerImpl {
 
     private static final Logger LOG = LoggerFactory.getLogger(PrestoJobManagerImpl.class);
+    private static final String PRESTO_PROTOCOL_KEY = "netflix.genie.server.presto.protocol";
+    private static final String PRESTO_MASTER_DOMAIN = "netflix.genie.server.presto.master.domain";
 
     /**
      * Constructor.
@@ -71,10 +73,23 @@ public class PrestoJobManagerImpl extends JobManagerImpl {
             throw new GeniePreconditionException("Init wasn't called. Unable to continue.");
         }
 
+        // Check the parameters
+        final String prestoProtocol = ConfigurationManager
+                .getConfigInstance().getString(PRESTO_PROTOCOL_KEY, null);
+        if (prestoProtocol == null) {
+            throw new GeniePreconditionException("Presto protocol not set. Please configure " + PRESTO_PROTOCOL_KEY);
+        }
+        final String prestoMasterDomain = ConfigurationManager
+                .getConfigInstance().getString(PRESTO_MASTER_DOMAIN, null);
+        if (prestoMasterDomain == null) {
+            throw new GeniePreconditionException("Presto protocol not set. Please configure " + PRESTO_MASTER_DOMAIN);
+        }
+
+
         // create the ProcessBuilder for this process
         final List<String> processArgs = this.createBaseProcessArguments();
         processArgs.add("--server");
-        processArgs.add("http://" + this.getCluster().getName() + ".master.dataeng.netflix.net:8080");
+        processArgs.add(PRESTO_PROTOCOL_KEY + this.getCluster().getName() + PRESTO_MASTER_DOMAIN);
         processArgs.add("--catalog");
         processArgs.add("hive");
         processArgs.add("--debug");
