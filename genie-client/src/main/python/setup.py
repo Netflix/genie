44f9d6e7e07cd07209ea6d47cf8ddca7edb56c3d
Tom Gianos <tgianos@netflix.com>
2015-02-13 11:44:47 -0800

    Updating the Python client in prep for 2.1.0 release

--- genie-client/src/main/python/setup.py
+++ genie-client/src/main/python/setup.py
@@ -26,7 +26,7 @@ with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 setup(
     name='nflx-genie-client',
 
-    version='2.0.0',
+    version='2.1.0',
 
     author='Netflix Inc.',
 
