09fbce53dc56b9ef0554fd87d4f65d75d3a68fdc
Tom Gianos <tgianos@netflix.com>
2015-08-21 13:26:29 -0700

    [#149] Added field search into elasticsearch for jobs

--- genie-server/src/main/java/com/netflix/genie/server/services/impl/elasticsearch/ESJobSearchServiceImpl.java
+++ genie-server/src/main/java/com/netflix/genie/server/services/impl/elasticsearch/ESJobSearchServiceImpl.java
@@ -22,8 +22,12 @@ import com.netflix.genie.common.model.Job;
 import com.netflix.genie.common.model.JobStatus;
 import com.netflix.genie.server.repository.elasticsearch.ESJobRepository;
 import com.netflix.genie.server.services.JobSearchService;
+import org.apache.commons.lang3.StringUtils;
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.Criteria;
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -39,15 +43,18 @@ import java.util.Set;
 public class ESJobSearchServiceImpl implements JobSearchService {
 
     private final ESJobRepository repository;
+    private final ElasticsearchTemplate template;
 
     /**
      * Constructor.
      *
-     * @param repository The elastic search repository.
+     * @param repository The elastic search repository
+     * @param template   The elastic search template
      */
     @Inject
-    public ESJobSearchServiceImpl(final ESJobRepository repository) {
+    public ESJobSearchServiceImpl(final ESJobRepository repository, final ElasticsearchTemplate template) {
         this.repository = repository;
+        this.template = template;
     }
 
     /**
@@ -77,7 +84,79 @@ public class ESJobSearchServiceImpl implements JobSearchService {
             final boolean descending,
             final Set<String> orderBys
     ) {
-        final PageRequest pageRequest = new PageRequest(page, limit);
-        return this.repository.findByTagsContains("kragle", pageRequest);
+        Criteria criteria = null;
+        if (StringUtils.isNotBlank(id)) {
+            criteria = new Criteria("id").contains(id);
+        }
+        if (StringUtils.isNotBlank(jobName)) {
+            if (criteria == null) {
+                criteria = new Criteria("name").contains(jobName);
+            } else {
+                criteria.and(new Criteria("name").contains(jobName));
+            }
+        }
+        if (StringUtils.isNotBlank(userName)) {
+            if (criteria == null) {
+                criteria = new Criteria("user").is(userName);
+            } else {
+                criteria.and(new Criteria("user").is(userName));
+            }
+        }
+        if (statuses != null && !statuses.isEmpty()) {
+            final Criteria statusCriteria = new Criteria("status");
+            statuses.stream().forEach(status -> statusCriteria.or(new Criteria("status").is(status)));
+
+            if (criteria == null) {
+                criteria = statusCriteria;
+            } else {
+                criteria.and(statusCriteria);
+            }
+        }
+        if (tags != null && !tags.isEmpty()) {
+            for (final String tag : tags) {
+                if (StringUtils.isNotBlank(tag)) {
+                    if (criteria == null) {
+                        criteria = new Criteria("tags").contains(tag);
+                    } else {
+                        criteria.and(new Criteria("tags").contains(tag));
+                    }
+                }
+            }
+        }
+        if (StringUtils.isNotBlank(clusterName)) {
+            if (criteria == null) {
+                criteria = new Criteria("executionClusterName").is(clusterName);
+            } else {
+                criteria.and("executionClusterName").is(clusterName);
+            }
+        }
+        if (StringUtils.isNotBlank(clusterId)) {
+            if (criteria == null) {
+                criteria = new Criteria("clusterId").is(clusterId);
+            } else {
+                criteria.and("clusterId").is(clusterId);
+            }
+        }
+        if (StringUtils.isNotBlank(commandName)) {
+            if (criteria == null) {
+                criteria = new Criteria("commandName").is(commandName);
+            } else {
+                criteria.and("commandName").is(commandName);
+            }
+        }
+        if (StringUtils.isNotBlank(commandId)) {
+            if (criteria == null) {
+                criteria = new Criteria("commandId").is(commandId);
+            } else {
+                criteria.and("commandId").is(commandId);
+            }
+        }
+
+        if (criteria != null) {
+            final CriteriaQuery query = new CriteriaQuery(criteria, new PageRequest(page, limit));
+            return this.template.queryForList(query, Job.class);
+        } else {
+            return this.repository.findAll(new PageRequest(page, limit)).getContent();
+        }
     }
 }
