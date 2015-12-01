82787c1b1b8e7ef7bcebc278e30e33ac0c138447
Amit Sharma <amitsharma1708@gmail.com>
2015-12-01 12:58:40 -0800

    [#149] Added create methods for job, jobexecution and jobrequest in jpa impl of job persistence service.

--- genie-core/src/main/java/com/netflix/genie/core/jpa/services/JpaJobPersistenceServiceImpl.java
+++ genie-core/src/main/java/com/netflix/genie/core/jpa/services/JpaJobPersistenceServiceImpl.java
@@ -19,11 +19,19 @@ package com.netflix.genie.core.jpa.services;
 
 import com.netflix.genie.common.dto.Job;
 
+import com.netflix.genie.common.dto.JobExecution;
+import com.netflix.genie.common.dto.JobRequest;
+import com.netflix.genie.common.exceptions.GenieConflictException;
 import com.netflix.genie.common.exceptions.GenieException;
 import com.netflix.genie.common.exceptions.GenieNotFoundException;
 import com.netflix.genie.core.jpa.entities.JobEntity;
+import com.netflix.genie.core.jpa.entities.JobRequestEntity;
+import com.netflix.genie.core.jpa.entities.JobExecutionEntity;
+import com.netflix.genie.core.jpa.repositories.JpaJobExecutionRepository;
 import com.netflix.genie.core.jpa.repositories.JpaJobRepository;
+import com.netflix.genie.core.jpa.repositories.JpaJobRequestRepository;
 import com.netflix.genie.core.services.JobPersistenceService;
+import org.apache.commons.lang3.StringUtils;
 import org.hibernate.validator.constraints.NotBlank;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,7 +39,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.validation.ConstraintViolationException;
 import javax.validation.constraints.NotNull;
+import java.util.UUID;
 
 /**
  * JPA implementation of the job persistence service.
@@ -39,21 +49,35 @@ import javax.validation.constraints.NotNull;
  * @author amsharma
  */
 @Service
+@Transactional(
+        rollbackFor = {
+                GenieException.class,
+                ConstraintViolationException.class
+        }
+)
 public class JpaJobPersistenceServiceImpl implements JobPersistenceService {
 
     private static final Logger LOG = LoggerFactory.getLogger(JpaJobPersistenceServiceImpl.class);
     private final JpaJobRepository jobRepo;
+    private final JpaJobRequestRepository jobRequestRepo;
+    private final JpaJobExecutionRepository jobExecutionRepo;
 
     /**
      * Default Constructor.
      *
      * @param jobRepo The job repository to use
+     * @param jobRequestRepo The job request repository to use
+     * @param jobExecutionRepo The jobExecution Repository to use
      */
     @Autowired
     public JpaJobPersistenceServiceImpl(
-            final JpaJobRepository jobRepo
+            final JpaJobRepository jobRepo,
+            final JpaJobRequestRepository jobRequestRepo,
+            final JpaJobExecutionRepository jobExecutionRepo
     ) {
         this.jobRepo = jobRepo;
+        this.jobRequestRepo = jobRequestRepo;
+        this.jobExecutionRepo = jobExecutionRepo;
     }
     /**
      {@inheritDoc}
@@ -74,11 +98,124 @@ public class JpaJobPersistenceServiceImpl implements JobPersistenceService {
      {@inheritDoc}
      */
     @Override
-    public void saveJob(
+    public void createJob(
             @NotNull(message = "Job is null so cannot be saved")
             final Job job
     ) throws GenieException {
 
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Called with job: " + job.toString());
+        }
+
+        // Since a job request object should always exist before the job object is saved
+        // the id should never be null
+        if (StringUtils.isBlank(job.getId())) {
+            throw new GenieConflictException("Cannot create a job with id blank or null");
+        }
+
+        if (this.jobRepo.exists(job.getId())) {
+            throw new GenieConflictException("A job with id " + job.getId() + " already exists");
+        }
+
+        final JobEntity jobEntity = new JobEntity();
+
+
+        jobEntity.setId(job.getId());
+        jobEntity.setName(job.getName());
+        jobEntity.setUser(job.getUser());
+        jobEntity.setVersion(job.getVersion());
+        jobEntity.setArchiveLocation(job.getArchiveLocation());
+        jobEntity.setDescription(job.getDescription());
+        jobEntity.setStarted(job.getStarted());
+        jobEntity.setStatus(job.getStatus());
+        jobEntity.setStatusMsg(job.getStatusMsg());
+
+        // TODO where are tags sorted?
+        jobEntity.setJobTags(job.getTags());
+
+        // TODO: where are the ones below set .. update method?
+        // command_id, command_name, cluster_id, cluster_name, exit_code, finished,
+        this.jobRepo.save(jobEntity);
+    }
+
+    /**
+     {@inheritDoc}
+     */
+    @Override
+    public String createJobRequest(
+            @NotNull(message = "Job Request is null so cannot be saved")
+            final JobRequest jobRequest
+    ) throws GenieException {
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Called with jobRequest: " + jobRequest.toString());
+        }
+
+        if (jobRequest.getId() != null && this.jobRequestRepo.exists(jobRequest.getId())) {
+            throw new GenieConflictException("A job with id " + jobRequest.getId() + " already exists");
+        }
+
+        final JobRequestEntity jobRequestEntity = new JobRequestEntity();
+
+        jobRequestEntity.setName(jobRequest.getName());
+        jobRequestEntity.setUser(jobRequest.getUser());
+        jobRequestEntity.setVersion(jobRequest.getVersion());
+        jobRequestEntity.setDescription(jobRequest.getDescription());
+        jobRequestEntity.setCommandArgs(jobRequest.getCommandArgs());
+        jobRequestEntity.setGroup(jobRequest.getGroup());
+        jobRequestEntity.setSetupFile(jobRequest.getSetupFile());
+        jobRequestEntity.setClusterCriteriasFromList(jobRequest.getClusterCriterias());
+        jobRequestEntity.setCommandCriteriaFromSet(jobRequest.getCommandCriteria());
+
+        // TODO convert set to csv
+        //jobRequestEntity.setFileDependencies(jobRequest.getFileDependencies());
+
+        jobRequestEntity.setDisableLogArchival(jobRequest.getDisableLogArchival());
+        jobRequestEntity.setEmail(jobRequest.getEmail());
+
+        // TODO sort tags?
+        jobRequestEntity.setTags(jobRequest.getTags());
+
+        jobRequestEntity.setCpu(jobRequest.getCpu());
+        jobRequestEntity.setMemory(jobRequest.getMemory());
+
+        // TODO client host should be part of jobRequest?
+        //jobRequestEntity.setClientHost(jobRequest.);
+
+        if (StringUtils.isBlank(jobRequestEntity.getId())) {
+            jobRequestEntity.setId(UUID.randomUUID().toString());
+        }
+
+        final String id = jobRequestEntity.getId();
+        this.jobRequestRepo.save(jobRequestEntity);
+        return id;
+    }
+
+    /**
+     {@inheritDoc}
+     */
+    @Override
+    public void createJobExecution(
+            @NotNull(message = "Job Request is null so cannot be saved")
+            final JobExecution jobExecution
+    ) throws GenieException {
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Called with jobExecution: " + jobExecution.toString());
+        }
+
+        if (jobExecution.getId() != null && this.jobExecutionRepo.exists(jobExecution.getId())) {
+            throw new GenieConflictException("A job with id " + jobExecution.getId() + " already exists");
+        }
+
+        final JobExecutionEntity jobExecutionEntity = new JobExecutionEntity();
+
+        jobExecutionEntity.setId(jobExecution.getId());
+        jobExecutionEntity.setClusterCriteriaFromSet(jobExecution.getClusterCriteria());
+        jobExecutionEntity.setHostName(jobExecution.getHostName());
+        jobExecutionEntity.setProcessId(jobExecution.getProcessId());
+
+        this.jobExecutionRepo.save(jobExecutionEntity);
     }
 
     /**
--- genie-core/src/main/java/com/netflix/genie/core/services/JobPersistenceService.java
+++ genie-core/src/main/java/com/netflix/genie/core/services/JobPersistenceService.java
@@ -19,6 +19,8 @@ package com.netflix.genie.core.services;
 
 import com.netflix.genie.common.dto.Job;
 
+import com.netflix.genie.common.dto.JobExecution;
+import com.netflix.genie.common.dto.JobRequest;
 import com.netflix.genie.common.exceptions.GenieException;
 import org.hibernate.validator.constraints.NotBlank;
 
@@ -48,10 +50,36 @@ public interface JobPersistenceService {
      * Save the job object in the data store.
      *
      * @param job the Job object to save
+     *
      * @throws GenieException if there is an error
      */
-    void saveJob(
+    void createJob(
             @NotNull(message = "Job is null so cannot be saved")
             final Job job
+    ) throws GenieException;
+
+    /**
+     * Save the jobRequest object in the data store.
+     *
+     * @param jobRequest the Job object to save
+     *
+     * @return The id of the jobReqest that was created
+     * @throws GenieException if there is an error
+     */
+    String createJobRequest(
+            @NotNull(message = "Job Request is null so cannot be saved")
+            final JobRequest jobRequest
+    ) throws GenieException;
+
+    /**
+     * Save the jobExecution object in the data store.
+     *
+     * @param jobExecution the Job object to save
+     *
+     * @throws GenieException if there is an error
+     */
+    void createJobExecution(
+            @NotNull(message = "Job Request is null so cannot be saved")
+            final JobExecution jobExecution
     ) throws GenieException;
 }
