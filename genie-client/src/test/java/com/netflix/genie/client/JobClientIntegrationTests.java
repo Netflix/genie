e5ec16ca7301f183e14ea809ab885dec7f064536
Amit Sharma <amitsharma1708@gmail.com>
2016-05-03 09:16:06 -0700

    Ignoring job client test.

--- genie-client/src/test/java/com/netflix/genie/client/JobClientIntegrationTests.java
+++ genie-client/src/test/java/com/netflix/genie/client/JobClientIntegrationTests.java
@@ -29,6 +29,7 @@ import com.netflix.genie.common.dto.JobExecution;
 import com.netflix.genie.common.dto.JobRequest;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -73,6 +74,7 @@ public class JobClientIntegrationTests extends GenieClientsIntegrationTestsBase 
      *
      * @throws Exception If there is any problem.
      */
+    @Ignore
     @Test
     public void canSubmitJob() throws Exception {
 
