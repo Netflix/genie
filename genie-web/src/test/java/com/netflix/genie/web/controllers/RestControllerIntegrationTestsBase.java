5dc7b65f89e44a442301c20d4945e65f9c72d708
Ajoy Majumdar <amajumdar@netflix.com>
2016-08-01 10:18:06 -0700

    Removed @DirtiesContext so as to not restarting the application across integration tests. Modified genie.file.cache.location to correctly point to /tmp.

--- genie-web/src/test/java/com/netflix/genie/web/controllers/RestControllerIntegrationTestsBase.java
+++ genie-web/src/test/java/com/netflix/genie/web/controllers/RestControllerIntegrationTestsBase.java
@@ -38,7 +38,6 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
