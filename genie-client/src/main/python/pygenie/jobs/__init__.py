5ebaf842bb04ee32fe8647dbca01c01adf809ba7
John Lee <johlee@netflix.com>
2017-07-31 11:47:24 -0700

    remove python client (moved to https://github.com/Netflix/pygenie)

--- genie-client/src/main/python/pygenie/jobs/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-from .core import GenieJob
-from .hadoop import HadoopJob
-from .hive import HiveJob
-from .pig import PigJob
-from .presto import PrestoJob
-from .running import RunningJob
-from .sqoop import SqoopJob
