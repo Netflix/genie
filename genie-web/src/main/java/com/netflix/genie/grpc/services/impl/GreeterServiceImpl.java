dcccb2a833c2992c7e1757d1e510958090e21c6c
Marco Primi <mprimi@netflix.com>
2017-09-25 10:42:26 -0700

    Add example gRPC service implementation and test

---
+++ genie-web/src/main/java/com/netflix/genie/grpc/services/impl/GreeterServiceImpl.java
@@ -0,0 +1,42 @@
+/*
+ *
+ *  Copyright 2016 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+ package com.netflix.genie.grpc.services.impl;
+
+import com.netflix.genie.grpc.SimpleLoggingInterceptor;
+import com.netflix.genie.protogen.GreeterServiceGrpc;
+import com.netflix.genie.protogen.HelloReply;
+import com.netflix.genie.protogen.HelloRequest;
+import io.grpc.stub.StreamObserver;
+
+/**
+ * Example implementation of the Greeter service definition.
+ */
+public class GreeterServiceImpl extends GreeterServiceGrpc.GreeterServiceImplBase {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void sayHello(final HelloRequest request, final StreamObserver<HelloReply> responseObserver) {
+        final HelloReply response = HelloReply.newBuilder()
+            .setMessage("Hello " + request.getName() + "!")
+            .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
