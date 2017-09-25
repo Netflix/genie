76245989d7cc9736bc9ce70a39114a90b321cb7b
Marco Primi <mprimi@netflix.com>
2017-09-25 10:36:54 -0700

    Set up minimal gRPC server configuration
     - Use net.devh:grpc-server-spring-boot-starter
     - Create new configuration (GRPCServerConfig)
     - Disable gRPC autoconfiguration unless explicitly enabled via properties
     - Enable gRPC server in 'dev' profile

---
+++ genie-web/src/main/java/com/netflix/genie/web/configs/GRPCServerConfig.java
@@ -0,0 +1,47 @@
+/*
+ *
+ *  Copyright 2017 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package com.netflix.genie.web.configs;
+
+import lombok.extern.slf4j.Slf4j;
+import net.devh.springboot.autoconfigure.grpc.server.GrpcServerAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Controls whether a gRPC server is configured for this Genie node or not.
+ *
+ * @author mprimi
+ * @since 4.0.0
+ */
+@Configuration
+@ConditionalOnProperty(value = "genie.grpc.server.enabled", havingValue = "true")
+@Import(GrpcServerAutoConfiguration.class)
+@Slf4j
+public class GRPCServerConfig {
+
+    /**
+     * Log that gRPC server is enabled.
+     */
+    @PostConstruct
+    public void postConstruct() {
+        log.info("gRPC server configuration is ENABLED");
+    }
+}
