c322dfd427b085e0f008a2625b303140043e8898
Marco Primi <mprimi@netflix.com>
2017-10-11 16:00:39 -0700

    Re-enable develop build but skip publishing SNAPSHOTs
     - Remove CI blacklisting of `develop` (so PR get validated)
     - Make sure `develop` does not produce and publish any artifacts

--- travis/buildViaTravis.sh
+++ travis/buildViaTravis.sh
@@ -4,6 +4,9 @@
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
   ./gradlew build asciidoc coveralls dockerBuildAllImages
+elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "develop" ]; then
+  echo -e 'Build Develop without publishing artifacts'
+  ./gradlew build
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
   ./gradlew -Prelease.travisci=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -PsonatypeUsername="${sonatypeUsername}" -PsonatypePassword="${sonatypePassword}" snapshot coveralls publishGhPages dockerPush
