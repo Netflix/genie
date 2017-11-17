4a6dc5606ba4ba3c32a4d9d1bc96bee4e2726e65
Marco Primi <mprimi@netflix.com>
2017-11-17 15:58:23 -0800

    Upgrade Nebula plugin and set release.travisBranch for Travis SNAPSHOT builds
    Setting release.travisBranch on a release branch (e.g., 3.2.x) creates snapshots with version that corresponds to the branch, rather than one that conflicts with the ones created by the master branch.

--- travis/buildViaTravis.sh
+++ travis/buildViaTravis.sh
@@ -13,7 +13,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   INTEGRATION_TEST_DB=postgresql ./gradlew genie-web:integrationTests
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-  ./gradlew -Prelease.travisci=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -PsonatypeUsername="${sonatypeUsername}" -PsonatypePassword="${sonatypePassword}" snapshot coveralls publishGhPages dockerPush
+  ./gradlew -Prelease.travisBranch=$TRAVIS_BRANCH -Prelease.travisci=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -PsonatypeUsername="${sonatypeUsername}" -PsonatypePassword="${sonatypePassword}" snapshot coveralls publishGhPages dockerPush
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
   case "$TRAVIS_TAG" in
