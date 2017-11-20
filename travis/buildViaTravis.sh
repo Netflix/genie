7199f0cb8be4c1805434e6d765c6780debfa32b4
Marco Primi <mprimi@netflix.com>
2017-11-20 14:57:04 -0800

    TEST

--- travis/buildViaTravis.sh
+++ travis/buildViaTravis.sh
@@ -6,7 +6,7 @@ set -e
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  ./gradlew --no-daemon build asciidoc coveralls dockerBuildAllImages
+  ./gradlew -Prelease.travisBranch=$TRAVIS_BRANCH --no-daemon build asciidoc coveralls dockerBuildAllImages
   # Re-run genie-web integration tests with MySQL...
   INTEGRATION_TEST_DB=mysql ./gradlew --no-daemon genie-web:integrationTests
   # ... and PostgreSQL
