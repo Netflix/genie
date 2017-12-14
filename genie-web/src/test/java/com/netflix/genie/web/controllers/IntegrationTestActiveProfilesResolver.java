a3273a69acc5c8064bef4f17a9d8d819d398bb8b
Tom Gianos <tgianos@netflix.com>
2017-12-13 16:35:09 -0800

    Switch from HSQLDB to H2 as in memory database implementation

--- genie-web/src/test/java/com/netflix/genie/web/controllers/IntegrationTestActiveProfilesResolver.java
+++ genie-web/src/test/java/com/netflix/genie/web/controllers/IntegrationTestActiveProfilesResolver.java
@@ -33,11 +33,11 @@ public class IntegrationTestActiveProfilesResolver implements ActiveProfilesReso
     private static final String DB_SELECTOR_ENV_VARIABLE_NAME = "INTEGRATION_TEST_DB";
     private static final String MYSQL = "mysql";
     private static final String POSTGRESQL = "postgresql";
-    private static final String HSQL_MEM = "hsql-mem";
+    private static final String H2 = "h2";
     private final Set<String> knownDatabaseProfiles = Sets.newHashSet(
         MYSQL,
         POSTGRESQL,
-        HSQL_MEM
+        H2
     );
 
     /**
@@ -45,13 +45,13 @@ public class IntegrationTestActiveProfilesResolver implements ActiveProfilesReso
      */
     @Override
     public String[] resolve(final Class<?> testClass) {
-        final String integrationTestDatabase = System.getProperty(DB_SELECTOR_ENV_VARIABLE_NAME, HSQL_MEM);
+        final String integrationTestDatabase = System.getProperty(DB_SELECTOR_ENV_VARIABLE_NAME, H2);
 
-        if (!knownDatabaseProfiles.contains(integrationTestDatabase)) {
+        if (!this.knownDatabaseProfiles.contains(integrationTestDatabase)) {
             throw new IllegalStateException("Unknown database profile: " + integrationTestDatabase);
         }
 
-        return new String[] {
+        return new String[]{
             "integration",
             "db",
             "db-" + integrationTestDatabase,
