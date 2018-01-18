2429f88bc8624cc169c44f80e44037f0b6a096ef
Tom Gianos <tgianos@netflix.com>
2018-01-17 22:07:33 -0800

    Add file service API for deleting unused files created before a given time

--- genie-web/src/test/java/com/netflix/genie/web/jpa/services/JpaFileServiceImplIntegrationTest.java
+++ genie-web/src/test/java/com/netflix/genie/web/jpa/services/JpaFileServiceImplIntegrationTest.java
@@ -18,10 +18,14 @@
 package com.netflix.genie.web.jpa.services;
 
 import com.github.springtestdbunit.annotation.DatabaseTearDown;
+import com.google.common.collect.Sets;
+import com.netflix.genie.common.dto.Application;
+import com.netflix.genie.common.dto.ApplicationStatus;
 import com.netflix.genie.common.exceptions.GenieException;
 import com.netflix.genie.test.categories.IntegrationTest;
 import com.netflix.genie.web.jpa.entities.FileEntity;
 import com.netflix.genie.web.jpa.repositories.JpaFileRepository;
+import com.netflix.genie.web.services.ApplicationService;
 import com.netflix.genie.web.services.FileService;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
@@ -29,6 +33,7 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -49,14 +54,15 @@ public class JpaFileServiceImplIntegrationTest extends DBUnitTestBase {
     @Autowired
     private JpaFileRepository fileRepository;
 
+    @Autowired
+    private ApplicationService applicationService;
+
     /**
      * Make sure that no matter how many times we try to create a file it doesn't throw an error on duplicate key it
      * just does nothing.
-     *
-     * @throws GenieException on error
      */
     @Test
-    public void canCreateFileIfNotExists() throws GenieException {
+    public void canCreateFileIfNotExists() {
         Assert.assertThat(this.fileRepository.count(), Matchers.is(0L));
         final String file = UUID.randomUUID().toString();
         this.fileService.createFileIfNotExists(file);
@@ -78,5 +84,52 @@ public class JpaFileServiceImplIntegrationTest extends DBUnitTestBase {
 
         // Make sure the ids are still equal
         Assert.assertThat(fileEntity2.getId(), Matchers.is(fileEntity.getId()));
+    }
+
+    /**
+     * Make sure we can delete files that aren't attached to other resources.
+     *
+     * @throws GenieException on error
+     */
+    @Test
+    public void canDeleteUnusedFiles() throws GenieException {
+        Assert.assertThat(this.fileRepository.count(), Matchers.is(0L));
+        final String file1 = UUID.randomUUID().toString();
+        final String file2 = UUID.randomUUID().toString();
+        final String file3 = UUID.randomUUID().toString();
+        final String file4 = UUID.randomUUID().toString();
+        final String file5 = UUID.randomUUID().toString();
+
+        this.fileService.createFileIfNotExists(file1);
+        this.fileService.createFileIfNotExists(file4);
+
+        final Application app = new Application.Builder(
+            UUID.randomUUID().toString(),
+            UUID.randomUUID().toString(),
+            UUID.randomUUID().toString(),
+            ApplicationStatus.ACTIVE
+        )
+            .withDependencies(Sets.newHashSet(file2))
+            .withConfigs(Sets.newHashSet(file3))
+            .withSetupFile(file5)
+            .build();
+
+        final String appId = this.applicationService.createApplication(app);
+
+        Assert.assertTrue(this.fileRepository.existsByFile(file1));
+        Assert.assertTrue(this.fileRepository.existsByFile(file2));
+        Assert.assertTrue(this.fileRepository.existsByFile(file3));
+        Assert.assertTrue(this.fileRepository.existsByFile(file4));
+        Assert.assertTrue(this.fileRepository.existsByFile(file5));
+
+        Assert.assertThat(this.fileService.deleteUnusedFiles(Instant.now()), Matchers.is(2));
+
+        Assert.assertFalse(this.fileRepository.existsByFile(file1));
+        Assert.assertTrue(this.fileRepository.existsByFile(file2));
+        Assert.assertTrue(this.fileRepository.existsByFile(file3));
+        Assert.assertFalse(this.fileRepository.existsByFile(file4));
+        Assert.assertTrue(this.fileRepository.existsByFile(file5));
+
+        this.applicationService.deleteApplication(appId);
     }
 }
