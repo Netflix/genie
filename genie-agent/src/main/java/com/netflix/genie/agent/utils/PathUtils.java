2e8fadfe63c846d328720efffa707e883711e7cb
Marco Primi <mprimi@netflix.com>
2018-02-23 15:09:18 -0800

    Add Environment and Path utility classes for Agent

---
+++ genie-agent/src/main/java/com/netflix/genie/agent/utils/PathUtils.java
@@ -0,0 +1,187 @@
+/*
+ *
+ *  Copyright 2018 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+
+package com.netflix.genie.agent.utils;
+
+import com.netflix.genie.common.jobs.JobConstants;
+
+import java.io.File;
+import java.nio.file.Path;
+
+/**
+ * Utilities to compose filesystem paths.
+ *
+ * @author mprimi
+ * @since 4.0.0
+ */
+public final class PathUtils {
+
+    private PathUtils() {
+    }
+
+    /**
+     * Append an arbitrary set of components to a base path.
+     * @param baseDirectory the base directory
+     * @param children path components
+     * @return a Path
+     */
+    public static Path composePath(
+        final File baseDirectory,
+        final String... children
+    ) {
+        File pathAccumulator = baseDirectory;
+        for (String child : children) {
+            pathAccumulator = new File(pathAccumulator, child);
+        }
+        return pathAccumulator.toPath();
+    }
+
+    /**
+     * Append an arbitrary set of components to a base path.
+     * @param baseDirectory the base directory
+     * @param children path components
+     * @return a Path
+     */
+    public static Path composePath(
+        final Path baseDirectory,
+        final String... children
+    ) {
+        return composePath(baseDirectory.toFile(), children);
+    }
+
+    /**
+     * Compose the path to an application directory inside a job directory.
+     * @param jobDirectory the job directory
+     * @param appId the application id
+     * @return a Path
+     */
+    public static Path jobApplicationDirectoryPath(
+        final File jobDirectory,
+        final String appId
+    ) {
+        return composePath(
+            jobDirectory,
+            JobConstants.GENIE_PATH_VAR,
+            JobConstants.APPLICATION_PATH_VAR,
+            appId
+        );
+    }
+
+    /**
+     * Compose the path to the cluster directory inside a job directory.
+     * @param jobDirectory the job directory
+     * @param clusterId the cluster id
+     * @return a Path
+     */
+    public static Path jobClusterDirectoryPath(final File jobDirectory, final String clusterId) {
+        return composePath(
+            jobDirectory,
+            JobConstants.GENIE_PATH_VAR,
+            JobConstants.CLUSTER_PATH_VAR,
+            clusterId
+        );
+    }
+
+    /**
+     * Compose the path to the command directory inside a job directory.
+     * @param jobDirectory the job directory
+     * @param commandId the command id
+     * @return a Path
+     */
+    public static Path jobCommandDirectoryPath(final File jobDirectory, final String commandId) {
+        return composePath(
+            jobDirectory,
+            JobConstants.GENIE_PATH_VAR,
+            JobConstants.COMMAND_PATH_VAR,
+            commandId
+        );
+    }
+
+    /**
+     * Compose the path to the genie directory inside a job directory.
+     * @param jobDirectory the job directory
+     * @return a Path
+     */
+    public static Path jobGenieDirectoryPath(final File jobDirectory) {
+        return composePath(
+            jobDirectory,
+            JobConstants.GENIE_PATH_VAR
+        );
+    }
+
+    /**
+     * Compose the path to the genie logs directory inside a job directory.
+     * @param jobDirectory the job directory
+     * @return a Path
+     */
+    public static Path jobGenieLogsDirectoryPath(final File jobDirectory) {
+        return composePath(
+            jobDirectory,
+            JobConstants.GENIE_PATH_VAR,
+            JobConstants.LOGS_PATH_VAR
+        );
+    }
+
+    /**
+     * Compose the path to the dependencies directory for a given entity.
+     * @param entityDirectory the entity base directory
+     * @return a Path
+     */
+    public static Path jobEntityDependenciesPath(final Path entityDirectory) {
+        return composePath(
+            new File(entityDirectory.toUri()),
+            JobConstants.DEPENDENCY_FILE_PATH_PREFIX
+        );
+    }
+
+    /**
+     * Compose the path to the configurations directory for a given entity.
+     * @param entityDirectory the entity base directory
+     * @return a Path
+     */
+    public static Path jobEntityConfigPath(final Path entityDirectory) {
+        return composePath(
+            new File(entityDirectory.toUri()),
+            JobConstants.CONFIG_FILE_PATH_PREFIX
+        );
+    }
+
+    /**
+     * Compose the path to the standard output log file for a job.
+     * @param jobDirectory the job directory
+     * @return a Path
+     */
+    public static Path jobStdOutPath(final File jobDirectory) {
+        return composePath(
+            jobDirectory,
+            JobConstants.STDOUT_LOG_FILE_NAME
+        );
+    }
+
+    /**
+     * Compose the path to the standard error log file for a job.
+     * @param jobDirectory the job directory
+     * @return a Path
+     */
+    public static Path jobStdErrPath(final File jobDirectory) {
+        return composePath(
+            jobDirectory,
+            JobConstants.STDERR_LOG_FILE_NAME
+        );
+    }
+}
---
+++ genie-agent/src/test/groovy/com/netflix/genie/agent/utils/EnvUtilsSpec.groovy
@@ -0,0 +1,123 @@
+/*
+ *
+ *  Copyright 2018 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+
+package com.netflix.genie.agent.utils
+
+import org.springframework.core.io.ClassPathResource
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardCopyOption
+
+class EnvUtilsSpec extends Specification {
+
+    static Map<String, String> emptyMap = [:]
+    static Map<String, String> fooBarMap = [
+            "foo": "bar",
+            "FOO": "BAR",
+            "F-O-O": "B A R"
+    ]
+    static Map<String, String> fooBarMultiLineMap = [
+            "foo": "bar",
+            "FOO": "BAR",
+            "multi-line": "1\n2\n3"
+    ]
+    static String testFile = EnvUtilsSpec.class.getSimpleName() + "-test1.txt"
+
+    @Unroll
+    def "parseEnvStream '#streamString'"(
+            String streamString,
+            Map<String, String> expectedValues
+    ) {
+        setup:
+        InputStream stream = new ByteArrayInputStream(streamString.getBytes(StandardCharsets.UTF_8))
+
+        when:
+        Map<String, String> parsedEnv = EnvUtils.parseEnvStream(stream)
+
+        then:
+        expectedValues.size() == parsedEnv.size()
+        expectedValues == parsedEnv
+
+        where:
+        streamString                                         | expectedValues
+        ""                                                   | emptyMap
+        "\n"                                                 | emptyMap
+        "\n \n"                                              | emptyMap
+        "foo='bar'\nFOO='BAR'\nF-O-O='B A R'\n"              | fooBarMap
+        "\nF-O-O='B A R'\nfoo='bar'\nFOO='BAR'"              | fooBarMap
+        "\nfoo='bar'\nFOO='BAR'\nF-O-O='B A R'\n"            | fooBarMap
+        "\n\nfoo='bar'\n\nFOO='BAR'\n\nF-O-O='B A R'\n\n"    | fooBarMap
+        "\nF-O-O='B A R'\nfoo='bar'\nFOO='BAR'\n"            | fooBarMap
+        "foo='bar'\nmulti-line='1\n2\n3'\n\nFOO='BAR'"       | fooBarMultiLineMap
+        "foo='bar'\nFOO='BAR'\nmulti-line='1\n2\n3'"         | fooBarMultiLineMap
+    }
+
+    @Unroll
+    def "parseEnvStream error '#streamString'"(
+            String streamString
+    ) {
+        setup:
+        InputStream stream = new ByteArrayInputStream(streamString.getBytes(StandardCharsets.UTF_8))
+
+        when:
+        EnvUtils.parseEnvStream(stream)
+
+        then:
+        thrown(EnvUtils.ParseException)
+
+        where:
+        streamString       | _
+        "foo"              | _
+        "foo\nfoo"         | _
+        "=foo"             | _
+        "foo='bar\nbar\n"  | _
+        "foo='bar\nbar'x"  | _
+    }
+
+    def "Parse nonexistent file"() {
+        when:
+        EnvUtils.parseEnvFile(new File("/this/file/does/not/exist"))
+
+        then:
+        thrown(IOException)
+    }
+
+    def "Parse file"() {
+        setup:
+        Path tempLocalFile = Files.createTempFile(null, null)
+        Files.copy(
+                new ClassPathResource(testFile).getInputStream(),
+                tempLocalFile,
+                StandardCopyOption.REPLACE_EXISTING
+        )
+
+        when:
+        Map<String, String> envMap = EnvUtils.parseEnvFile(tempLocalFile.toFile())
+
+        then:
+        42 == envMap.size()
+        "w0t1p0:E3EAD46A-721E-4B1C-A56F-4D9C2EE0595E" == envMap.get("ITERM_SESSION_ID")
+        "" == envMap.get("n")
+        "/usr/local/bin/mate -w" == envMap.get("EDITOR")
+        "foo\nbar" == envMap.get("multiline")
+    }
+}
