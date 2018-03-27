39932ec6daac366008e808d455c4e795f5c9a7bc
Marco Primi <mprimi@netflix.com>
2018-03-27 13:31:39 -0700

    Add argument converters for type Criteria and JsonNode

--- genie-agent/src/main/java/com/netflix/genie/agent/cli/ArgumentConverters.java
+++ genie-agent/src/main/java/com/netflix/genie/agent/cli/ArgumentConverters.java
@@ -20,10 +20,20 @@ package com.netflix.genie.agent.cli;
 
 import com.beust.jcommander.IStringConverter;
 import com.beust.jcommander.ParameterException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.collect.Sets;
+import com.netflix.genie.common.dto.v4.Criterion;
+import com.netflix.genie.common.exceptions.GeniePreconditionException;
+import com.netflix.genie.common.util.GenieObjectMapper;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Utilities to convert arguments during parsing.
@@ -41,6 +51,10 @@ final class ArgumentConverters {
     static final class FileConverter implements IStringConverter<File> {
         @Override
         public File convert(final String value) {
+
+            if (StringUtils.isBlank(value)) {
+                throw new ParameterException("Invalid file: '" + value + "'");
+            }
             return new File(value);
         }
     }
@@ -52,6 +66,78 @@ final class ArgumentConverters {
                 return new URI(value);
             } catch (final URISyntaxException e) {
                 throw new ParameterException("Invalid URI: " + value, e);
+            }
+        }
+    }
+
+    static final class CriterionConverter implements IStringConverter<Criterion> {
+
+        static final String CRITERION_SYNTAX_MESSAGE = "CRITERION SYNTAX:\n"
+            + "Criterion is parsed as a string in the format:\n"
+            + "    ID=i/NAME=n/STATUS=s/TAGS=t1,t2,t3\n"
+            + "Note:\n"
+            + " - All components (ID, NAME, STATUS, TAGS) are optional, but at least one is required\n"
+            + " - Order of components is enforced (i.e. NAME cannot appear before ID)\n"
+            + " - Values cannot be empty (skip the components altogether if no value is present)\n";
+
+        private static final Pattern SINGLE_CRITERION_PATTERN = Pattern.compile(
+            "^ID=(?<id>[^/]+)|NAME=(?<name>[^/]+)|STATUS=(?<status>[^/]+)|TAGS=(?<tags>[^/]+)$"
+        );
+
+        private static final Pattern MULTI_CRITERION_PATTERN = Pattern.compile(
+            "^(ID=(?<id>[^/]+)/)?(NAME=(?<name>[^/]+))?/?(STATUS=(?<status>[^/]+))?/?(TAGS=(?<tags>[^/]+))?$"
+        );
+
+        private static final String ID_CAPTURE_GROUP = "id";
+        private static final String NAME_CAPTURE_GROUP = "name";
+        private static final String STATUS_CAPTURE_GROUP = "status";
+        private static final String TAGS_CAPTURE_GROUP = "tags";
+
+        @Override
+        public Criterion convert(final String value) {
+
+            final Criterion.Builder criterionBuilder = new Criterion.Builder();
+
+            final Matcher multiComponentMatcher = MULTI_CRITERION_PATTERN.matcher(value);
+            final Matcher singleComponentMatcher = SINGLE_CRITERION_PATTERN.matcher(value);
+
+            final Matcher matchingMatcher;
+
+            if (multiComponentMatcher.matches()) {
+                matchingMatcher = multiComponentMatcher;
+            } else if (singleComponentMatcher.matches()) {
+                matchingMatcher = singleComponentMatcher;
+            } else {
+                throw new ParameterException("Invalid criterion: " + value);
+            }
+
+            final String id = matchingMatcher.group(ID_CAPTURE_GROUP);
+            final String name = matchingMatcher.group(NAME_CAPTURE_GROUP);
+            final String status = matchingMatcher.group(STATUS_CAPTURE_GROUP);
+            final String tags = matchingMatcher.group(TAGS_CAPTURE_GROUP);
+            final Set<String> splitTags = tags == null ? null : Sets.newHashSet(tags.split(","));
+
+            criterionBuilder
+                .withId(id)
+                .withName(name)
+                .withStatus(status)
+                .withTags(splitTags);
+
+            try {
+                return criterionBuilder.build();
+            } catch (final GeniePreconditionException e) {
+                throw new ParameterException("Invalid criterion: " + value, e);
+            }
+        }
+    }
+
+    static final class JSONConverter implements IStringConverter<JsonNode> {
+        @Override
+        public JsonNode convert(final String value) {
+            try {
+                return GenieObjectMapper.getMapper().readTree(value);
+            } catch (final IOException e) {
+                throw new ParameterException("Failed to parse JSON argument", e);
             }
         }
     }
