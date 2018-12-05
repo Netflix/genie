be6a336a8c99392494ac0837005ec890a3bb31ce
Marco Primi <mprimi@netflix.com>
2018-12-05 11:30:16 -0800

    Make active jobs user limit value customizable on a per-user basis
    The existing mechanism to limit the maximum number of jobs applies equally to all users.
    This can result in having to configure its value to a 'known accepted worst case'. And that limit is then applied to everyone else.
    
    The new mechanism allows to set a default value that applies to most users, plus a set of custom limits for individual users.
    These limits can be higher or lower than the default.

--- genie-core/src/main/java/com/netflix/genie/core/properties/JobsUsersActiveLimitProperties.java
+++ genie-core/src/main/java/com/netflix/genie/core/properties/JobsUsersActiveLimitProperties.java
@@ -17,11 +17,13 @@
  */
 package com.netflix.genie.core.properties;
 
+import com.google.common.collect.Maps;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.Min;
+import java.util.Map;
 
 /**
  * Properties related to user limits in number of active jobs.
@@ -46,4 +48,16 @@ public class JobsUsersActiveLimitProperties {
     private boolean enabled = DEFAULT_ENABLED;
     @Min(value = 1)
     private int count = DEFAULT_COUNT;
+    private Map<String, Integer> overrides = Maps.newHashMap();
+
+    /**
+     * Get the maximum number of jobs a user is allowed to run concurrently.
+     * Checks whether the user has a special limit associated, and if not it returns the global default.
+     *
+     * @param user the user name
+     * @return the maximum number of jobs
+     */
+    public int getUserLimit(final String user) {
+        return overrides.getOrDefault(user, count);
+    }
 }
