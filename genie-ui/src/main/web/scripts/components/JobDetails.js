4639189155bf36a3bd128b4693b6fd3737438a96
Abhishek Kapatkar <cabhishek@gmail.com>
2019-03-21 17:22:29 -0700

    Upgrade Jobs details view with more data points
    Following fields removed:
    
    * Command name
    * Command version
    * Command created
    * Command updated
    * Command status
    
    Following fields added:
    
    * Add link to Cluster Id
    * Add link to application Id
    * Add 'grouping'
    * Add 'groupingInstance'

--- genie-ui/src/main/web/scripts/components/JobDetails.js
+++ genie-ui/src/main/web/scripts/components/JobDetails.js
@@ -6,6 +6,8 @@ import { momentFormat, fetch } from "../utils";
 import Modal from "react-modal";
 import Error from "./Error";
 
+import $ from "jquery";
+
 export default class JobDetails extends React.Component {
   static propTypes = { row: T.object.isRequired };
 
@@ -16,6 +18,8 @@ export default class JobDetails extends React.Component {
       modalIsOpen: false,
       killRequestError: null,
       command: { id: "", version: "", name: "" },
+      cluster: { id: "", version: "", name: "" },
+      applications: [],
       job: {
         id: "",
         tags: [],
@@ -31,6 +35,7 @@ export default class JobDetails extends React.Component {
         }
       }
     };
+    window.myState = this.state;
   }
 
   componentDidMount() {
@@ -46,9 +51,19 @@ export default class JobDetails extends React.Component {
     const url = row._links.self.href;
     fetch(url).done(job => {
       this.setState({ job });
-      fetch(job._links.command.href)
-        .done(command => {
-          this.setState({ command });
+
+      $.when(
+        fetch(job._links.cluster.href),
+        fetch(job._links.command.href),
+        fetch(job._links.applications.href)
+      )
+        .done((clusters, commands, applications) => {
+          console.log(applications);
+          this.setState({
+            cluster: clusters[0],
+            command: commands[0],
+            applications: applications[0]
+          });
         })
         .fail(xhr => {
           this.setState({ error: xhr.responseJSON });
@@ -101,63 +116,69 @@ export default class JobDetails extends React.Component {
               <tbody>
                 <tr>
                   <td className="col-xs-2 align-right">Job Id:</td>
-                  <td>
-                    {this.state.job.id}
-                  </td>
+                  <td>{this.state.job.id}</td>
                 </tr>
                 <tr>
                   <td className="col-xs-2 align-right">Version:</td>
-                  <td>
-                    {this.state.job.version}
-                  </td>
+                  <td>{this.state.job.version}</td>
                 </tr>
                 <tr>
                   <td className="col-xs-2 align-right">Description:</td>
-                  <td>
-                    {this.state.job.description}
-                  </td>
+                  <td>{this.state.job.description}</td>
                 </tr>
                 <tr>
                   <td className="col-xs-2 align-right">Tags:</td>
                   <td>
-                    {this.state.job.tags.join(",")}
-                  </td>
-                </tr>
-                <tr>
-                  <td className="col-xs-2 align-right">Status:</td>
-                  <td>
-                    {this.state.job.status}
+                    <ul>
+                      {this.state.job.tags.map((tag, index) => (
+                        <li key={index}>{tag}</li>
+                      ))}
+                    </ul>
                   </td>
                 </tr>
                 <tr>
                   <td className="col-xs-2 align-right">Status Msg:</td>
-                  <td>
-                    {this.state.job.statusMsg}
-                  </td>
+                  <td>{this.state.job.statusMsg}</td>
                 </tr>
                 <tr>
                   <td className="col-xs-2 align-right">Created:</td>
-                  <td>
-                    {momentFormat(this.state.job.created)}
-                  </td>
+                  <td>{momentFormat(this.state.job.created)}</td>
                 </tr>
                 <tr>
                   <td className="col-xs-2 align-right">Updated:</td>
-                  <td>
-                    {momentFormat(this.state.job.updated)}
-                  </td>
+                  <td>{momentFormat(this.state.job.updated)}</td>
                 </tr>
                 <tr>
                   <td className="col-xs-2 align-right">Archive Location:</td>
+                  <td>{this.state.job.archiveLocation}</td>
+                </tr>
+                <tr>
+                  <td className="col-xs-2 align-right">Application Ids:</td>
                   <td>
-                    {this.state.job.archiveLocation}
+                    <ul>
+                      {this.state.applications.map((application, index) => (
+                        <li key={index}>
+                          <Link
+                            to={`applications?name=${application.name}&rowId=${
+                              application.id
+                            }`}
+                          >
+                            {application.id}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
                   </td>
                 </tr>
                 <tr>
-                  <td className="col-xs-2 align-right">Command Name:</td>
+                  <td className="col-xs-2 align-right">Cluster Id:</td>
                   <td>
-                    <Link to={`commands?name=${this.state.job.commandName}`}>
-                      {this.state.job.commandName}
+                    <Link
+                      to={`clusters?name=${this.state.job.clusterName}&rowId=${
+                        this.state.cluster.id
+                      }`}
+                    >
+                      {this.state.cluster.id}
                     </Link>
                   </td>
                 </tr>
@@ -165,42 +186,25 @@ export default class JobDetails extends React.Component {
                   <td className="col-xs-2 align-right">Command Id:</td>
                   <td>
                     <Link
-                      to={`commands?name=${this.state.job
-                        .commandName}&rowId=${this.state.command.id}`}
+                      to={`commands?name=${this.state.job.commandName}&rowId=${
+                        this.state.command.id
+                      }`}
                     >
                       {this.state.command.id}
                     </Link>
                   </td>
                 </tr>
                 <tr>
-                  <td className="col-xs-2 align-right">Command Version:</td>
-                  <td>
-                    {this.state.command.version}
-                  </td>
-                </tr>
-                <tr>
-                  <td className="col-xs-2 align-right">Command Created:</td>
-                  <td>
-                    {momentFormat(this.state.command.created)}
-                  </td>
-                </tr>
-                <tr>
-                  <td className="col-xs-2 align-right">Command Updated:</td>
-                  <td>
-                    {momentFormat(this.state.command.updated)}
-                  </td>
-                </tr>
-                <tr>
-                  <td className="col-xs-2 align-right">Command Status:</td>
-                  <td>
-                    {this.state.command.status}
-                  </td>
-                </tr>
-                <tr>
                   <td className="col-xs-2 align-right">Command Args:</td>
-                  <td>
-                    {this.state.job.commandArgs}
-                  </td>
+                  <td>{this.state.job.commandArgs}</td>
+                </tr>
+                <tr>
+                  <td className="col-xs-2 align-right">Grouping:</td>
+                  <td>{this.state.job.grouping}</td>
+                </tr>
+                <tr>
+                  <td className="col-xs-2 align-right">Grouping Instance:</td>
+                  <td>{this.state.job.groupingInstance}</td>
                 </tr>
                 <tr>
                   <td className="col-xs-2 align-right">Details:</td>
@@ -243,86 +247,81 @@ export default class JobDetails extends React.Component {
                 </tr>
                 {(this.state.job.status === "RUNNING" ||
                   this.state.job.status === "INIT") &&
-                !this.state.killJobRequestSent
-                  ? <tr>
-                      <td>
-                        <button
-                          type="button"
-                          className="btn btn-danger"
-                          onClick={this.openModal}
-                        >
-                          Send Kill Request
-                        </button>
-                        <Modal
-                          isOpen={this.state.modalIsOpen}
-                          onRequestClose={this.closeModal}
-                          style={this.customStyles}
-                        >
-                          <div>
-                            <div className="modal-header">
-                              <h4
-                                className="modal-title"
-                                id="alert-modal-label"
-                              >
-                                Confirm Kill Request
-                              </h4>
-                            </div>
-                            <div className="modal-body">
-                              <div>This cannot be undone.</div>
-                            </div>
-                            <div className="modal-footer">
-                              <button
-                                type="button"
-                                className="btn btn-primary"
-                                onClick={() => this.killJob(this.state.job.id)}
-                              >
-                                OK
-                              </button>
-                              <button
-                                type="button"
-                                className="btn btn-default"
-                                onClick={this.closeModal}
-                              >
-                                Cancel
-                              </button>
-                            </div>
+                !this.state.killJobRequestSent ? (
+                  <tr>
+                    <td>
+                      <button
+                        type="button"
+                        className="btn btn-danger"
+                        onClick={this.openModal}
+                      >
+                        Send Kill Request
+                      </button>
+                      <Modal
+                        isOpen={this.state.modalIsOpen}
+                        onRequestClose={this.closeModal}
+                        style={this.customStyles}
+                      >
+                        <div>
+                          <div className="modal-header">
+                            <h4 className="modal-title" id="alert-modal-label">
+                              Confirm Kill Request
+                            </h4>
                           </div>
-                        </Modal>
-                      </td>
-                    </tr>
-                  : null}
+                          <div className="modal-body">
+                            <div>This cannot be undone.</div>
+                          </div>
+                          <div className="modal-footer">
+                            <button
+                              type="button"
+                              className="btn btn-primary"
+                              onClick={() => this.killJob(this.state.job.id)}
+                            >
+                              OK
+                            </button>
+                            <button
+                              type="button"
+                              className="btn btn-default"
+                              onClick={this.closeModal}
+                            >
+                              Cancel
+                            </button>
+                          </div>
+                        </div>
+                      </Modal>
+                    </td>
+                  </tr>
+                ) : null}
               </tbody>
             </table>
-            {this.state.killJobRequestSent && !this.state.killRequestError
-              ? <small>
-                  *Request accepted. Please refresh the page in a few seconds to
-                  see the status change.
-                </small>
-              : null}
-            {this.state.killRequestError
-              ? <div>
-                  <div>
-                    <small>
-                      *Request failed. Please refresh the page and try again.
-                    </small>
-                  </div>
-                  <div>
-                    <small>
-                      <code>
-                        {this.state.killRequestError.message}.
-                      </code>
-                    </small>
-                  </div>
+            {this.state.killJobRequestSent && !this.state.killRequestError ? (
+              <small>
+                *Request accepted. Please refresh the page in a few seconds to
+                see the status change.
+              </small>
+            ) : null}
+            {this.state.killRequestError ? (
+              <div>
+                <div>
+                  <small>
+                    *Request failed. Please refresh the page and try again.
+                  </small>
                 </div>
-              : null}
-            {this.state.error
-              ? <div>
-                  <div>
-                    <span>Failed to load Command details:</span>
-                  </div>
-                  <Error error={this.state.error} />
+                <div>
+                  <small>
+                    <code>{this.state.killRequestError.message}.</code>
+                  </small>
                 </div>
-              : null}
+              </div>
+            ) : null}
+            {this.state.error ? (
+              <div>
+                <div>
+                  <span>Failed to load Command details:</span>
+                </div>
+                <Error error={this.state.error} />
+              </div>
+            ) : null}
           </div>
         </td>
       </tr>
