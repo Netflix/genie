7fa6756d75b48c89763983fbd407a5d814c2a100
Tom Gianos <tgianos@netflix.com>
2019-07-22 15:14:33 -0700

    Add ResolvedJob class which will be used to contain all resolved information

---
+++ genie-web/src/main/java/com/netflix/genie/web/dtos/ResolvedJob.java
@@ -0,0 +1,42 @@
+/*
+ *
+ *  Copyright 2019 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package com.netflix.genie.web.dtos;
+
+import com.netflix.genie.common.internal.dto.v4.JobEnvironment;
+import com.netflix.genie.common.internal.dto.v4.JobSpecification;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+/**
+ * The payload of information representing all the concrete details the system needs to run a job.
+ *
+ * @author tgianos
+ * @since 4.0.0
+ */
+@RequiredArgsConstructor
+@Getter
+@EqualsAndHashCode(doNotUseGetters = true)
+@ToString(doNotUseGetters = true)
+@SuppressWarnings("FinalClass")
+public class ResolvedJob {
+
+    private final JobSpecification jobSpecification;
+    private final JobEnvironment jobEnvironment;
+}
---
+++ genie-web/src/test/groovy/com/netflix/genie/web/dtos/ResolvedJobSpec.groovy
@@ -0,0 +1,54 @@
+/*
+ *
+ *  Copyright 2019 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package com.netflix.genie.web.dtos
+
+import com.netflix.genie.common.internal.dto.v4.JobEnvironment
+import com.netflix.genie.common.internal.dto.v4.JobSpecification
+import spock.lang.Specification
+
+/**
+ * Specifications for {@link ResolvedJob}.
+ *
+ * @author tgianos
+ */
+class ResolvedJobSpec extends Specification {
+
+    def "can create and do all POJO operations"() {
+        def jobSpecification = Mock(JobSpecification)
+        def jobEnvironment = Mock(JobEnvironment)
+
+        when:
+        def resolvedJob = new ResolvedJob(jobSpecification, jobEnvironment)
+
+        then:
+        resolvedJob.getJobSpecification() == jobSpecification
+        resolvedJob.getJobEnvironment() == jobEnvironment
+
+        when:
+        def resolvedJob2 = new ResolvedJob(Mock(JobSpecification), Mock(JobEnvironment))
+        def resolvedJob3 = new ResolvedJob(jobSpecification, jobEnvironment)
+
+        then:
+        resolvedJob != resolvedJob2
+        resolvedJob == resolvedJob3
+        resolvedJob.hashCode() != resolvedJob2.hashCode()
+        resolvedJob.hashCode() == resolvedJob3.hashCode()
+        resolvedJob.toString() != resolvedJob2.toString()
+        resolvedJob.toString() == resolvedJob3.toString()
+    }
+}
