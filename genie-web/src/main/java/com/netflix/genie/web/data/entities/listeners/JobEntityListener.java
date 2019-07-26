5131aef757a71c9af98ba70c03658ba971f5ed5f
Marco Primi <mprimi@netflix.com>
2019-07-26 15:16:22 -0700

    Create EntityListener for Job which can track persisted mutations to the entity
    Currently only tracks 'status' and delivers significant changes to a specialized observer.

---
+++ genie-web/src/main/java/com/netflix/genie/web/data/entities/listeners/JobEntityListener.java
@@ -0,0 +1,98 @@
+/*
+ *
+ *  Copyright 2019 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package com.netflix.genie.web.data.entities.listeners;
+
+import com.netflix.genie.common.dto.JobStatus;
+import com.netflix.genie.web.data.entities.JobEntity;
+import com.netflix.genie.web.data.observers.PersistedJobStatusObserver;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.PostLoad;
+import javax.persistence.PostUpdate;
+
+/**
+ * Listener for Job JPA entity ({@link JobEntity}).
+ * Currently tracks persistent changes to the status of a job and notifies an observer.
+ * Could be extended to do proxy more persist changes.
+ * <p>
+ * N.B. Spring configuration.
+ * - This class does not appear in any AutoConfiguration as bean.
+ * It is referenced as {@link java.util.EventListener} by {@link JobEntity}.
+ * EntityManager creates it even if a bean of the same type exists already.
+ * - The constructor parameter {@code persistedJobStatusObserver} is marked {@code Nullable} so that an instance can be
+ * created even if no bean of that type is configured.
+ *
+ * @author mprimi
+ * @since 4.0.0
+ */
+@Slf4j
+public class JobEntityListener {
+
+    private final PersistedJobStatusObserver persistedJobStatusObserver;
+
+    /**
+     * Constructor.
+     *
+     * @param persistedJobStatusObserver the observer to notify of persisted job status changes
+     */
+    public JobEntityListener(
+        final PersistedJobStatusObserver persistedJobStatusObserver
+    ) {
+        this.persistedJobStatusObserver = persistedJobStatusObserver;
+    }
+
+    /**
+     * Persistence callback invoked after a job entity has been committed/flushed into persistent storage.
+     * If the persisted status of the job is different from the last one notified, then a notification is emitted.
+     *
+     * @param jobEntity the job that was just persisted
+     */
+    @PostUpdate
+    public void jobUpdate(final JobEntity jobEntity) {
+
+        final JobStatus currentState = jobEntity.getStatus();
+        final JobStatus previouslyNotifiedState = jobEntity.getNotifiedJobStatus();
+        final String jobId = jobEntity.getUniqueId();
+        if (currentState != previouslyNotifiedState) {
+            log.info(
+                "Detected state change for job: {} from: {} to: {}",
+                jobId,
+                previouslyNotifiedState,
+                currentState
+            );
+
+            // Notify observer
+            this.persistedJobStatusObserver.notify(jobId, previouslyNotifiedState, currentState);
+
+            // Save this as the latest published state
+            jobEntity.setNotifiedJobStatus(currentState);
+        }
+    }
+
+    /**
+     * Persistence callback invoked after a job entity is loaded or refreshed.
+     * The job status loaded from persistent storage is also the last state that was notified.
+     *
+     * @param jobEntity the job that was just loaded
+     */
+    @PostLoad
+    public void jobLoad(final JobEntity jobEntity) {
+        // The persisted status is also the most recently notified state.
+        jobEntity.setNotifiedJobStatus(jobEntity.getStatus());
+    }
+}
---
+++ genie-web/src/test/groovy/com/netflix/genie/web/data/entities/listeners/JobEntityListenerSpec.groovy
@@ -0,0 +1,78 @@
+/*
+ *
+ *  Copyright 2019 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package com.netflix.genie.web.data.entities.listeners
+
+import com.netflix.genie.common.dto.JobStatus
+import com.netflix.genie.web.data.entities.JobEntity
+import com.netflix.genie.web.data.observers.PersistedJobStatusObserver
+import spock.lang.Specification
+
+class JobEntityListenerSpec extends Specification {
+    String jobId
+    PersistedJobStatusObserver observer
+    JobEntity jobEntity
+
+    void setup() {
+
+        this.jobId = UUID.randomUUID().toString()
+        this.observer = Mock(PersistedJobStatusObserver)
+        this.jobEntity = Mock(JobEntity)
+    }
+
+    def "Can track job status and notify the observer"() {
+        setup:
+        JobEntityListener listener = new JobEntityListener(observer)
+
+        when:
+        listener.jobUpdate(jobEntity)
+
+        then:
+        1 * jobEntity.getStatus() >> JobStatus.RESERVED
+        1 * jobEntity.getNotifiedJobStatus() >> null
+        1 * jobEntity.getUniqueId() >> jobId
+        1 * observer.notify(jobId, null, JobStatus.RESERVED)
+        1 * jobEntity.setNotifiedJobStatus(JobStatus.RESERVED)
+
+        when:
+        listener.jobLoad(jobEntity)
+
+        then:
+        1 * jobEntity.getStatus() >> JobStatus.RESERVED
+        1 * jobEntity.setNotifiedJobStatus(JobStatus.RESERVED)
+
+        when:
+        listener.jobUpdate(jobEntity)
+
+        then:
+        1 * jobEntity.getStatus() >> JobStatus.RESOLVED
+        1 * jobEntity.getNotifiedJobStatus() >> JobStatus.RESERVED
+        1 * jobEntity.getUniqueId() >> jobId
+        1 * observer.notify(jobId, JobStatus.RESERVED, JobStatus.RESOLVED)
+        1 * jobEntity.setNotifiedJobStatus(JobStatus.RESOLVED)
+
+        when:
+        listener.jobUpdate(jobEntity)
+
+        then:
+        1 * jobEntity.getStatus() >> JobStatus.RUNNING
+        1 * jobEntity.getNotifiedJobStatus() >> JobStatus.RUNNING
+        1 * jobEntity.getUniqueId() >> jobId
+        0 * observer.notify(_, _, _)
+        0 * jobEntity.setNotifiedJobStatus(_)
+    }
+}
