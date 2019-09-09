c4734948e4ea5e0e66b62475f1fd1945a78674aa
Tom Gianos <tgianos@netflix.com>
2019-09-09 14:32:38 -0700

    Add JobProcessResult class
    To be used within Agent to return final metadata about the job subprocess to interested parties.
    Encapsulated within our own class to allow for extensibility later as we find more information we may want to capture and return.

---
+++ genie-agent/src/main/java/com/netflix/genie/agent/execution/process/JobProcessResult.java
@@ -0,0 +1,113 @@
+/*
+ *
+ *  Copyright 2019 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package com.netflix.genie.agent.execution.process;
+
+import com.netflix.genie.common.dto.JobStatus;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * A DTO POJO to capture final information about the job process this agent process was responsible for.
+ *
+ * @author tgianos
+ * @since 4.0.0
+ */
+@Getter
+@EqualsAndHashCode(doNotUseGetters = true)
+@ToString(doNotUseGetters = true)
+@SuppressWarnings("FinalClass")
+public class JobProcessResult {
+
+    private final JobStatus finalStatus;
+    private final String finalStatusMessage;
+    private final long stdOutSize;
+    private final long stdErrSize;
+
+    private JobProcessResult(final Builder builder) {
+        this.finalStatus = builder.bFinalStatus;
+        this.finalStatusMessage = builder.bFinalStatusMessage;
+        this.stdOutSize = builder.bStdOutSize;
+        this.stdErrSize = builder.bStdErrSize;
+    }
+
+    /**
+     * A builder to create valid, immutable {@link JobProcessResult} instances.
+     *
+     * @author tgianos
+     * @since 4.0.0
+     */
+    public static class Builder {
+        private final JobStatus bFinalStatus;
+        private final String bFinalStatusMessage;
+        private long bStdOutSize;
+        private long bStdErrSize;
+
+        /**
+         * Constructor.
+         *
+         * @param finalStatus        The final {@link JobStatus} for the job. {@link JobStatus#isFinished()} must return
+         *                           true
+         * @param finalStatusMessage The final human readable message for the job status
+         * @throws IllegalArgumentException When {@literal finalStatus} is not a final status
+         */
+        public Builder(final JobStatus finalStatus, final String finalStatusMessage) throws IllegalArgumentException {
+            if (!finalStatus.isFinished()) {
+                throw new IllegalArgumentException(
+                    "finalStatus must be one of the final states: "
+                        + JobStatus.getFinishedStatuses()
+                        + ". Was "
+                        + finalStatusMessage
+                );
+            }
+            this.bFinalStatus = finalStatus;
+            this.bFinalStatusMessage = finalStatusMessage;
+        }
+
+        /**
+         * Set the length of the std out file in bytes if there was one.
+         *
+         * @param stdOutSize The length of the std out file in bytes
+         * @return This builder object
+         */
+        public Builder withStdOutSize(final long stdOutSize) {
+            this.bStdOutSize = Math.max(stdOutSize, 0L);
+            return this;
+        }
+
+        /**
+         * Set the length of the std error file in bytes if there was one.
+         *
+         * @param stdErrSize The length of the std error file in bytes
+         * @return This builder object
+         */
+        public Builder withStdErrSize(final long stdErrSize) {
+            this.bStdErrSize = Math.max(stdErrSize, 0L);
+            return this;
+        }
+
+        /**
+         * Create a new immutable {@link JobProcessResult} instance based on the current contents of this builder.
+         *
+         * @return A {@link JobProcessResult} instance
+         */
+        public JobProcessResult build() {
+            return new JobProcessResult(this);
+        }
+    }
+}
---
+++ genie-agent/src/test/groovy/com/netflix/genie/agent/execution/process/JobProcessResultSpec.groovy
@@ -0,0 +1,65 @@
+/*
+ *
+ *  Copyright 2019 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package com.netflix.genie.agent.execution.process
+
+import com.netflix.genie.common.dto.JobStatus
+import com.netflix.genie.common.dto.JobStatusMessages
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * Specifications for {@link JobProcessResult}.
+ *
+ * @author tgianos
+ */
+class JobProcessResultSpec extends Specification {
+
+    @Unroll
+    def "can create with #finalStatus, #finalStatusMessage, #stdOutSize, #stdErrSize"() {
+        when:
+        def builder = new JobProcessResult.Builder(finalStatus, finalStatusMessage)
+        if (stdOutSize != null) {
+            builder.withStdOutSize(stdOutSize)
+        }
+        if (stdErrSize != null) {
+            builder.withStdErrSize(stdErrSize)
+        }
+        def result = builder.build()
+
+        then:
+        result.getFinalStatus() == finalStatus
+        result.getFinalStatusMessage() == finalStatusMessage
+        result.getStdOutSize() == expectedStdOutSize
+        result.getStdErrSize() == expectedStdErrSize
+
+        where:
+        finalStatus         | finalStatusMessage                          | stdOutSize | expectedStdOutSize | stdErrSize | expectedStdErrSize
+        JobStatus.SUCCEEDED | JobStatusMessages.JOB_FINISHED_SUCCESSFULLY | -1L        | 0L                 | 10L        | 10L
+        JobStatus.FAILED    | JobStatusMessages.JOB_FAILED                | 1L         | 1L                 | -45L       | 0L
+        JobStatus.KILLED    | JobStatusMessages.JOB_KILLED_BY_USER        | null       | 0L                 | null       | 0L
+        JobStatus.INVALID   | "I was bad"                                 | null       | 0L                 | null       | 0L
+    }
+
+    def "Preconditions throw expected exceptions"() {
+        when:
+        new JobProcessResult.Builder(JobStatus.RUNNING, "This will fail")
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+}
