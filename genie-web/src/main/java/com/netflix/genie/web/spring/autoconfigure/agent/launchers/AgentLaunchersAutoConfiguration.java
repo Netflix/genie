39ce573169f7fef9c71af44d907810e7001fada6
Tom Gianos <tgianos@netflix.com>
2019-09-23 14:56:34 -0700

    Add auto configuration for available agent launchers

---
+++ genie-web/src/main/java/com/netflix/genie/web/spring/autoconfigure/agent/launchers/AgentLaunchersAutoConfiguration.java
@@ -0,0 +1,89 @@
+/*
+ *
+ *  Copyright 2019 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package com.netflix.genie.web.spring.autoconfigure.agent.launchers;
+
+import com.netflix.genie.web.agent.launchers.AgentLauncher;
+import com.netflix.genie.web.agent.launchers.impl.LocalAgentLauncherImpl;
+import com.netflix.genie.web.data.services.JobSearchService;
+import com.netflix.genie.web.dtos.GenieWebHostInfo;
+import com.netflix.genie.web.dtos.GenieWebRpcInfo;
+import com.netflix.genie.web.properties.LocalAgentLauncherProperties;
+import com.netflix.genie.web.util.ExecutorFactory;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Auto configuration for beans responsible ofor launching Genie Agent instances.
+ *
+ * @author tgianos
+ * @since 4.0.0
+ */
+@Configuration
+@EnableConfigurationProperties(
+    {
+        LocalAgentLauncherProperties.class
+    }
+)
+public class AgentLaunchersAutoConfiguration {
+
+    /**
+     * Provide an {@link ExecutorFactory} instance if no other was defined.
+     *
+     * @return Instance of {@link ExecutorFactory}
+     */
+    @Bean
+    @ConditionalOnMissingBean(ExecutorFactory.class)
+    public ExecutorFactory processExecutorFactory() {
+        return new ExecutorFactory();
+    }
+
+    /**
+     * Provide a {@link AgentLauncher} implementation which launches local agent processes if no other implementation
+     * is defined.
+     *
+     * @param genieWebHostInfo   The {@link GenieWebHostInfo} of this instance
+     * @param genieWebRpcInfo    The {@link GenieWebRpcInfo} of this instance
+     * @param jobSearchService   The {@link JobSearchService} instance to use
+     * @param launcherProperties The properties related to launching an agent locally
+     * @param executorFactory    The {@link ExecutorFactory} to use to launch agent processes
+     * @param registry           The {@link MeterRegistry} to register metrics
+     * @return A {@link LocalAgentLauncherImpl} instance
+     */
+    @Bean
+    @ConditionalOnMissingBean(AgentLauncher.class)
+    public LocalAgentLauncherImpl localAgentLauncher(
+        final GenieWebHostInfo genieWebHostInfo,
+        final GenieWebRpcInfo genieWebRpcInfo,
+        final JobSearchService jobSearchService,
+        final LocalAgentLauncherProperties launcherProperties,
+        final ExecutorFactory executorFactory,
+        final MeterRegistry registry
+    ) {
+        return new LocalAgentLauncherImpl(
+            genieWebHostInfo,
+            genieWebRpcInfo,
+            jobSearchService,
+            launcherProperties,
+            executorFactory,
+            registry
+        );
+    }
+}
---
+++ genie-web/src/test/java/com/netflix/genie/web/spring/autoconfigure/agent/launchers/AgentLaunchersAutoConfigurationTest.java
@@ -0,0 +1,115 @@
+/*
+ *
+ *  Copyright 2019 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package com.netflix.genie.web.spring.autoconfigure.agent.launchers;
+
+import com.netflix.genie.web.agent.launchers.impl.LocalAgentLauncherImpl;
+import com.netflix.genie.web.data.services.JobSearchService;
+import com.netflix.genie.web.dtos.GenieWebHostInfo;
+import com.netflix.genie.web.dtos.GenieWebRpcInfo;
+import com.netflix.genie.web.properties.LocalAgentLauncherProperties;
+import com.netflix.genie.web.util.ExecutorFactory;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.annotation.Bean;
+
+import java.util.UUID;
+
+/**
+ * Tests for {@link AgentLaunchersAutoConfiguration}.
+ *
+ * @author tgianos
+ * @since 4.0.0
+ */
+public class AgentLaunchersAutoConfigurationTest {
+
+    private ApplicationContextRunner contextRunner =
+        new ApplicationContextRunner()
+            .withConfiguration(
+                AutoConfigurations.of(
+                    AgentLaunchersAutoConfiguration.class
+                )
+            )
+            .withUserConfiguration(UserConfig.class);
+
+    /**
+     * All the expected beans should exist when the auto configuration is applied.
+     */
+    @Test
+    public void testExpectedBeansExist() {
+        this.contextRunner.run(
+            context -> {
+                Assertions.assertThat(context).hasSingleBean(LocalAgentLauncherProperties.class);
+                final LocalAgentLauncherProperties properties = context.getBean(LocalAgentLauncherProperties.class);
+                Assertions.assertThat(properties.getExecutable()).isNotEmpty();
+                Assertions
+                    .assertThat(properties.getExecutable())
+                    .containsExactly("java", "-jar", "/tmp/genie-agent.jar");
+                Assertions.assertThat(context).hasSingleBean(ExecutorFactory.class);
+                Assertions.assertThat(context).hasSingleBean(LocalAgentLauncherImpl.class);
+            }
+        );
+    }
+
+    /**
+     * Make sure the property is completely overwritten for the executable.
+     */
+    @Test
+    public void testOverrideExecutable() {
+        this.contextRunner
+            .withPropertyValues("genie.agent.launcher.local.executable=blah,/tmp/genie-agent-2.jar")
+            .run(
+                context -> {
+                    Assertions.assertThat(context).hasSingleBean(LocalAgentLauncherProperties.class);
+                    final LocalAgentLauncherProperties properties = context.getBean(LocalAgentLauncherProperties.class);
+                    Assertions.assertThat(properties.getExecutable()).isNotEmpty();
+                    Assertions
+                        .assertThat(properties.getExecutable())
+                        .containsExactly("blah", "/tmp/genie-agent-2.jar");
+                    Assertions.assertThat(context).hasSingleBean(ExecutorFactory.class);
+                    Assertions.assertThat(context).hasSingleBean(LocalAgentLauncherImpl.class);
+                }
+            );
+    }
+
+    static class UserConfig {
+        @Bean
+        public GenieWebHostInfo genieWebHostInfo() {
+            return new GenieWebHostInfo(UUID.randomUUID().toString());
+        }
+
+        @Bean
+        public GenieWebRpcInfo genieWebRpcInfo() {
+            return new GenieWebRpcInfo(33_433);
+        }
+
+        @Bean
+        public JobSearchService jobSearchService() {
+            return Mockito.mock(JobSearchService.class);
+        }
+
+        @Bean
+        public MeterRegistry meterRegistry() {
+            return new SimpleMeterRegistry();
+        }
+    }
+}
