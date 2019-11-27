4fc8d2a967ae37717400e0809368e9c2e423aa3d
Marco Primi <mprimi@netflix.com>
2019-11-27 10:38:12 -0800

    New checked exceptions for Script subsystem
    Represent failures to load and run a script.
    Or an attempt to execute a script that is not configured.

---
+++ genie-web/src/main/java/com/netflix/genie/web/exceptions/checked/ScriptNotConfiguredException.java
@@ -0,0 +1,64 @@
+/*
+ *
+ *  Copyright 2019 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package com.netflix.genie.web.exceptions.checked;
+
+import com.netflix.genie.common.internal.exceptions.checked.GenieCheckedException;
+
+/**
+ * An exception thrown when a script bean is not configured an thus it cannot be loaded or executed.
+ *
+ * @author mprimi
+ * @since 4.0.0
+ */
+public class ScriptNotConfiguredException extends GenieCheckedException {
+
+    /**
+     * Constructor.
+     */
+    public ScriptNotConfiguredException() {
+        super();
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param message The detail message
+     */
+    public ScriptNotConfiguredException(final String message) {
+        super(message);
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param message The detail message
+     * @param cause   The root cause of this exception
+     */
+    public ScriptNotConfiguredException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param cause The root cause of this exception
+     */
+    public ScriptNotConfiguredException(final Throwable cause) {
+        super(cause);
+    }
+}
