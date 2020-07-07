a547bbf1eb2dd6174c016017425f1275965b0a11
Marco Primi <mprimi@netflix.com>
2020-07-07 15:20:54 -0700

    Customize Agent shutdown sequence
    Customize task executors and task schedulers such that when the Agent JVM begins the shutdown process, the running tasks have some leeway to complete (for example, update the final job status, archive the files) before Spring starts shutting down.
    
     * Introduce new properties for shutdown of schedulers and executors
     * Configure internal executors/schedulers as well as the Spring ones

---
+++ genie-agent/src/test/groovy/com/netflix/genie/agent/properties/ShutdownPropertiesSpec.groovy
@@ -0,0 +1,53 @@
+/*
+ *
+ *  Copyright 2020 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package com.netflix.genie.agent.properties
+
+import spock.lang.Specification
+
+import java.time.Duration
+
+class ShutdownPropertiesSpec extends Specification {
+
+
+    ShutdownProperties shutdownProperties
+
+    void setup() {
+        shutdownProperties = new ShutdownProperties()
+    }
+
+    def "defaults, getters setters"() {
+        expect:
+        shutdownProperties.getInternalExecutorsLeeway() == Duration.ofSeconds(30)
+        shutdownProperties.getInternalSchedulersLeeway() == Duration.ofSeconds(30)
+        shutdownProperties.getSystemExecutorLeeway() == Duration.ofSeconds(60)
+        shutdownProperties.getSystemSchedulerLeeway() == Duration.ofSeconds(60)
+
+        when:
+
+        shutdownProperties.setInternalExecutorsLeeway(Duration.ofSeconds(1))
+        shutdownProperties.setInternalSchedulersLeeway(Duration.ofSeconds(2))
+        shutdownProperties.setSystemExecutorLeeway(Duration.ofSeconds(3))
+        shutdownProperties.setSystemSchedulerLeeway(Duration.ofSeconds(4))
+
+        then:
+        shutdownProperties.getInternalExecutorsLeeway() == Duration.ofSeconds(1)
+        shutdownProperties.getInternalSchedulersLeeway() == Duration.ofSeconds(2)
+        shutdownProperties.getSystemExecutorLeeway() == Duration.ofSeconds(3)
+        shutdownProperties.getSystemSchedulerLeeway() == Duration.ofSeconds(4)
+    }
+}
