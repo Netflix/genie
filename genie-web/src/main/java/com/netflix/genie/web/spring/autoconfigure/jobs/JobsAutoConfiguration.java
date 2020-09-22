b1949aa336a91ce1d73fef534ff2dd2f3897f138
Marco Primi <mprimi@netflix.com>
2020-09-22 10:08:15 -0700

    Delete ProcessChecker and related classes

--- genie-web/src/main/java/com/netflix/genie/web/spring/autoconfigure/jobs/JobsAutoConfiguration.java
+++ genie-web/src/main/java/com/netflix/genie/web/spring/autoconfigure/jobs/JobsAutoConfiguration.java
@@ -17,15 +17,9 @@
  */
 package com.netflix.genie.web.spring.autoconfigure.jobs;
 
-import com.netflix.genie.web.properties.JobsProperties;
 import com.netflix.genie.web.scripts.ExecutionModeFilterScript;
 import com.netflix.genie.web.util.JobExecutionModeSelector;
-import com.netflix.genie.web.util.ProcessChecker;
-import com.netflix.genie.web.util.UnixProcessChecker;
 import io.micrometer.core.instrument.MeterRegistry;
-import org.apache.commons.exec.Executor;
-import org.apache.commons.lang3.SystemUtils;
-import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -42,29 +36,6 @@ import java.util.Optional;
 @Configuration
 // TODO: This is going to go away once the V4 API is in place
 public class JobsAutoConfiguration {
-
-    /**
-     * Create a {@link ProcessChecker.Factory} suitable for UNIX systems.
-     *
-     * @param executor       The executor where checks are executed
-     * @param jobsProperties The jobs properties
-     * @return a {@link ProcessChecker.Factory}
-     */
-    @Bean
-    @ConditionalOnMissingBean(ProcessChecker.Factory.class)
-    public ProcessChecker.Factory processCheckerFactory(
-        final Executor executor,
-        final JobsProperties jobsProperties
-    ) {
-        if (SystemUtils.IS_OS_UNIX) {
-            return new UnixProcessChecker.Factory(
-                executor,
-                jobsProperties.getUsers().isRunAsUserEnabled()
-            );
-        } else {
-            throw new BeanCreationException("No implementation available for non-UNIX systems");
-        }
-    }
 
     /**
      * Create a {@link JobExecutionModeSelector} if one does not exist.
