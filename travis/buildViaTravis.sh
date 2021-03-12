a79958f792344a309fedbd31a9641256e3f729a1
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2021-03-12 11:39:36 -0800

    Use Github Actions for publishing

--- travis/buildViaTravis.sh
+++ travis/buildViaTravis.sh
@@ -17,23 +17,4 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   INTEGRATION_TEST_DB=postgresql ${GRADLE} ${GRADLE_OPTIONS} genie-web:integrationTest
   # Build Docker images and compile documentation
   ${GRADLE} ${GRADLE_OPTIONS} javadoc asciidoc dockerBuildAllImages
-elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ] && [ "$TRAVIS_BRANCH" == "dev-snapshot" ]; then
-  echo -e 'Build Development Snapshot'
-  ${GRADLE} ${GRADLE_OPTIONS} --debug -Prelease.travisBranch=$TRAVIS_BRANCH -Prelease.travisci=true -PnetflixOss.username=$NETFLIX_OSS_REPO_USERNAME -PnetflixOss.password=$NETFLIX_OSS_REPO_PASSWORD  -Psonatype.signingPassword=$NETFLIX_OSS_SIGNING_PASSWORD snapshot dockerPush -x check
-elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
-  echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-  ${GRADLE} ${GRADLE_OPTIONS} --debug -Prelease.travisBranch=$TRAVIS_BRANCH -Prelease.travisci=true -PnetflixOss.username=$NETFLIX_OSS_REPO_USERNAME -PnetflixOss.password=$NETFLIX_OSS_REPO_PASSWORD  -Psonatype.signingPassword=$NETFLIX_OSS_SIGNING_PASSWORD snapshot codeCoverageReport coveralls gitPublishPush dockerPush
-elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
-  echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-  case "$TRAVIS_TAG" in
-  *-rc\.*)
-    ${GRADLE} ${GRADLE_OPTIONS} -Prelease.travisci=true -Prelease.useLastTag=true  -PnetflixOss.username=$NETFLIX_OSS_REPO_USERNAME -PnetflixOss.password=$NETFLIX_OSS_REPO_PASSWORD  -Psonatype.signingPassword=$NETFLIX_OSS_SIGNING_PASSWORD candidate codeCoverageReport coveralls gitPublishPush dockerPush
-    ;;
-  *)
-    ${GRADLE} ${GRADLE_OPTIONS} -Prelease.travisci=true -Prelease.useLastTag=true  -PnetflixOss.username=$NETFLIX_OSS_REPO_USERNAME -PnetflixOss.password=$NETFLIX_OSS_REPO_PASSWORD  -Psonatype.signingPassword=$NETFLIX_OSS_SIGNING_PASSWORD final codeCoverageReport coveralls gitPublishPush dockerPush
-    ;;
-  esac
-else
-  echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
-  ${GRADLE} ${GRADLE_OPTIONS} build codeCoverageReport coveralls
 fi
