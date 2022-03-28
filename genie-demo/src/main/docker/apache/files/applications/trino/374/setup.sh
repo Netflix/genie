b40d1e72151c8542ceebb357b5f603a9fa916204
Tom Gianos <tgianos@netflix.com>
2022-03-28 11:15:30 -0700

    Fix Demo
    Presto split into presto and trino. Looks like the presto containers are gone from docker. Switch to Trino.
    
    Related to #1164

--- genie-demo/src/main/docker/apache/files/applications/trino/374/setup.sh
+++ genie-demo/src/main/docker/apache/files/applications/trino/374/setup.sh
@@ -4,10 +4,10 @@ set -o errexit -o nounset -o pipefail
 
 START_DIR=`pwd`
 cd `dirname ${BASH_SOURCE[0]}`
-PRESTO_BASE=`pwd`
-cd ${START_DIR}
+TRINO_BASE=`pwd`
+cd "${START_DIR}"
 
-chmod 755 ${PRESTO_BASE}/dependencies/presto-cli-337-executable.jar
+chmod 755 "${TRINO_BASE}"/dependencies/trino-cli-374-executable.jar
 
 # Set the cli path for the commands to use when they invoke presto using this Application
-export PRESTO_CLI="${PRESTO_BASE}/dependencies/presto-cli-337-executable.jar"
+export TRINO_CLI="${TRINO_BASE}/dependencies/trino-cli-374-executable.jar"
