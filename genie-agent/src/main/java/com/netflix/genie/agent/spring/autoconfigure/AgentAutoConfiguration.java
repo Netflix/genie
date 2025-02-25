87d5849e6a071d37f478a49c5023e10d745f7611
Binbing Hou <bhou@netflix.com>
2025-02-25 11:11:56 -0800

    Revert the change to AgentAutoConfiguration and addressing the missing packages of TaskExecutorCustomizer and TaskSchedulerCustomizer

--- genie-agent/src/main/java/com/netflix/genie/agent/spring/autoconfigure/AgentAutoConfiguration.java
+++ genie-agent/src/main/java/com/netflix/genie/agent/spring/autoconfigure/AgentAutoConfiguration.java
@@ -23,9 +23,10 @@ import com.netflix.genie.agent.properties.AgentProperties;
 import com.netflix.genie.agent.utils.locks.impl.FileLockFactory;
 import com.netflix.genie.common.internal.util.GenieHostInfo;
 import com.netflix.genie.common.internal.util.HostnameUtil;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.task.TaskExecutorCustomizer;
+import org.springframework.boot.task.TaskSchedulerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -37,33 +38,37 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import java.net.UnknownHostException;
 
 /**
- * Configuration class for setting up agent-related beans.
+ * Configuration for various agent beans.
+ *
+ * @author standon
+ * @since 4.0.0
  */
 @Configuration
-@EnableConfigurationProperties(AgentProperties.class)
+@EnableConfigurationProperties(
+    {
+        AgentProperties.class
+    }
+)
 public class AgentAutoConfiguration {
 
     /**
-     * Provides the GenieHostInfo bean.
+     * Provide a bean of type {@link GenieHostInfo} if none already exists.
      *
-     * @return a GenieHostInfo object containing the hostname
-     * @throws UnknownHostException if the hostname cannot be determined
+     * @return A {@link GenieHostInfo} instance
+     * @throws UnknownHostException if hostname cannot be determined
      */
     @Bean
     @ConditionalOnMissingBean(GenieHostInfo.class)
     public GenieHostInfo genieAgentHostInfo() throws UnknownHostException {
         final String hostname = HostnameUtil.getHostname();
-        if (StringUtils.isBlank(hostname)) {
-            throw new IllegalStateException("Unable to determine hostname.");
-        }
         return new GenieHostInfo(hostname);
     }
 
     /**
-     * Provides the AgentMetadata bean.
+     * Provide a lazy bean definition for {@link AgentMetadata} if none already exists.
      *
-     * @param genieHostInfo the GenieHostInfo containing the hostname
-     * @return an instance of AgentMetadataImpl
+     * @param genieHostInfo the host information
+     * @return A {@link AgentMetadataImpl} instance
      */
     @Bean
     @Lazy
@@ -73,9 +78,9 @@ public class AgentAutoConfiguration {
     }
 
     /**
-     * Provides the FileLockFactory bean.
+     * Provide a lazy {@link FileLockFactory}.
      *
-     * @return a new instance of FileLockFactory
+     * @return A {@link FileLockFactory} instance
      */
     @Bean
     @Lazy
@@ -84,10 +89,11 @@ public class AgentAutoConfiguration {
     }
 
     /**
-     * Provides a shared AsyncTaskExecutor bean.
+     * Get a lazy {@link AsyncTaskExecutor} bean which may be shared by different components if one isn't already
+     * defined.
      *
-     * @param agentProperties the properties for configuring the agent
-     * @return a configured ThreadPoolTaskExecutor
+     * @param agentProperties the agent properties
+     * @return A {@link ThreadPoolTaskExecutor} instance
      */
     @Bean
     @Lazy
@@ -100,36 +106,35 @@ public class AgentAutoConfiguration {
         executor.setAwaitTerminationSeconds(
             (int) agentProperties.getShutdown().getInternalExecutorsLeeway().getSeconds()
         );
-        executor.initialize();
         return executor;
     }
 
     /**
-     * Provides a shared TaskScheduler bean.
+     * Provide a lazy {@link TaskScheduler} to be used by the Agent process if one isn't already defined.
      *
-     * @param agentProperties the properties for configuring the agent
-     * @return a configured ThreadPoolTaskScheduler
+     * @param agentProperties the agent properties
+     * @return A {@link ThreadPoolTaskScheduler} instance
      */
     @Bean
     @Lazy
     @ConditionalOnMissingBean(name = "sharedAgentTaskScheduler")
     public TaskScheduler sharedAgentTaskScheduler(final AgentProperties agentProperties) {
         final ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
-        scheduler.setPoolSize(5);
+        scheduler.setPoolSize(5); // Big enough?
         scheduler.setThreadNamePrefix("agent-task-scheduler-");
         scheduler.setWaitForTasksToCompleteOnShutdown(true);
         scheduler.setAwaitTerminationSeconds(
             (int) agentProperties.getShutdown().getInternalSchedulersLeeway().getSeconds()
         );
-        scheduler.initialize();
         return scheduler;
     }
 
     /**
-     * Provides a TaskScheduler bean for the heart beat service.
+     * Provide a lazy {@link TaskScheduler} bean for use by the heart beat service is none has already been
+     * defined in the context.
      *
-     * @param agentProperties the properties for configuring the agent
-     * @return a configured ThreadPoolTaskScheduler for heart beat service
+     * @param agentProperties the agent properties
+     * @return A {@link TaskScheduler} that the heart beat service should use
      */
     @Bean
     @Lazy
@@ -137,12 +142,43 @@ public class AgentAutoConfiguration {
     public TaskScheduler heartBeatServiceTaskScheduler(final AgentProperties agentProperties) {
         final ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
         taskScheduler.setPoolSize(1);
-        taskScheduler.setThreadNamePrefix("heart-beat-scheduler-");
+        taskScheduler.initialize();
         taskScheduler.setWaitForTasksToCompleteOnShutdown(true);
         taskScheduler.setAwaitTerminationSeconds(
             (int) agentProperties.getShutdown().getInternalSchedulersLeeway().getSeconds()
         );
-        taskScheduler.initialize();
         return taskScheduler;
+    }
+
+    /**
+     * Customizer for Spring's task executor.
+     *
+     * @param agentProperties the agent properties
+     * @return a customizer for the task executor
+     */
+    @Bean
+    TaskExecutorCustomizer taskExecutorCustomizer(final AgentProperties agentProperties) {
+        return taskExecutor -> {
+            taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
+            taskExecutor.setAwaitTerminationSeconds(
+                (int) agentProperties.getShutdown().getSystemExecutorLeeway().getSeconds()
+            );
+        };
+    }
+
+    /**
+     * Customizer for Spring's task scheduler.
+     *
+     * @param agentProperties the agent properties
+     * @return a customizer for the task scheduler
+     */
+    @Bean
+    TaskSchedulerCustomizer taskSchedulerCustomizer(final AgentProperties agentProperties) {
+        return taskScheduler -> {
+            taskScheduler.setWaitForTasksToCompleteOnShutdown(true);
+            taskScheduler.setAwaitTerminationSeconds(
+                (int) agentProperties.getShutdown().getSystemSchedulerLeeway().getSeconds()
+            );
+        };
     }
 }
