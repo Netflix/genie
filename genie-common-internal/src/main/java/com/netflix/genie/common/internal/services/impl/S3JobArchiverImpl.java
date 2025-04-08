4210b61c2342062c8adddf9c7989786c15386979
Binbing Hou <bhou@netflix.com>
2025-04-08 14:49:55 -0700

    Upgrade S3JobArchiverImpl

--- genie-common-internal/src/main/java/com/netflix/genie/common/internal/services/impl/S3JobArchiverImpl.java
+++ genie-common-internal/src/main/java/com/netflix/genie/common/internal/services/impl/S3JobArchiverImpl.java
@@ -17,15 +17,16 @@
  */
 package com.netflix.genie.common.internal.services.impl;
 
-import com.amazonaws.services.s3.AmazonS3URI;
-import com.amazonaws.services.s3.transfer.MultipleFileUpload;
-import com.amazonaws.services.s3.transfer.TransferManager;
 import com.netflix.genie.common.internal.aws.s3.S3ClientFactory;
 import com.netflix.genie.common.internal.exceptions.checked.JobArchiveException;
-import com.netflix.genie.common.internal.services.JobArchiveService;
 import com.netflix.genie.common.internal.services.JobArchiver;
 import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Uri;
+import software.amazon.awssdk.services.s3.S3Utilities;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.core.sync.RequestBody;
 
 import java.io.File;
 import java.net.URI;
@@ -33,16 +34,14 @@ import java.nio.file.Path;
 import java.util.List;
 
 /**
- * Implementation of {@link JobArchiveService} for S3 destinations.
- *
- * @author standon
- * @author tgianos
- * @since 4.0.0
+ * Implementation of {@link JobArchiver} for S3 destinations.
  */
 @Slf4j
 public class S3JobArchiverImpl implements JobArchiver {
 
     private final S3ClientFactory s3ClientFactory;
+
+    private final S3Utilities s3Utilities;
 
     /**
      * Constructor.
@@ -51,6 +50,7 @@ public class S3JobArchiverImpl implements JobArchiver {
      */
     public S3JobArchiverImpl(final S3ClientFactory s3ClientFactory) {
         this.s3ClientFactory = s3ClientFactory;
+        this.s3Utilities = S3Utilities.builder().build();
     }
 
     /**
@@ -63,36 +63,52 @@ public class S3JobArchiverImpl implements JobArchiver {
         @NotNull final URI target
     ) throws JobArchiveException {
         final String uriString = target.toString();
-        final AmazonS3URI s3URI;
-        try {
-            s3URI = new AmazonS3URI(target);
-        } catch (final IllegalArgumentException iae) {
-            log.debug("{} is not a valid S3 URI", uriString);
-            return false;
-        }
-        final String directoryString = directory.toString();
+        final S3Uri s3Uri = s3Utilities.parseUri(target);
+
+        final String bucketName = String.valueOf(s3Uri.bucket());
+        final String keyPrefix = String.valueOf(s3Uri.key());
+
         log.debug(
             "{} is a valid S3 location. Proceeding to archive {} to location: {}",
             uriString,
-            directoryString,
+            directory,
             uriString
         );
 
         try {
-            final TransferManager transferManager = this.s3ClientFactory.getTransferManager(s3URI);
+            final S3Client s3Client = this.s3ClientFactory.getClient(s3Uri);
 
-            final MultipleFileUpload upload = transferManager.uploadFileList(
-                s3URI.getBucket(),
-                s3URI.getKey(),
-                directory.toFile(),
-                filesList
-            );
+            for (File file : filesList) {
+                String key = keyPrefix + "/" + directory.relativize(file.toPath()).toString();
+                PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .build();
 
-            upload.waitForCompletion();
+                s3Client.putObject(putObjectRequest, RequestBody.fromFile(file.toPath()));
+            }
             return true;
         } catch (final Exception e) {
             log.error("Error archiving to S3 location: {} ", uriString, e);
-            throw new JobArchiveException("Error archiving " + directoryString, e);
+            throw new JobArchiveException("Error archiving " + directory.toString(), e);
         }
+    }
+
+    private String extractBucketName(URI uri) {
+        // Implement logic to extract bucket name from URI
+        String host = uri.getHost();
+        if (host == null) {
+            throw new IllegalArgumentException("Invalid S3 URI: " + uri);
+        }
+        return host;
+    }
+
+    private String extractKeyPrefix(URI uri) {
+        // Implement logic to extract key prefix from URI
+        String path = uri.getPath();
+        if (path.startsWith("/")) {
+            path = path.substring(1);
+        }
+        return path;
     }
 }
