cac4d53437c070f51101b2347ed528730437cc41
Binbing Hou <bhou@netflix.com>
2025-04-08 18:38:05 -0700

    remove unused imports

--- genie-web/src/main/java/com/netflix/genie/web/spring/autoconfigure/aws/AWSAutoConfiguration.java
+++ genie-web/src/main/java/com/netflix/genie/web/spring/autoconfigure/aws/AWSAutoConfiguration.java
@@ -33,10 +33,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.time.Duration;
 
 /**
  * AWS beans.
