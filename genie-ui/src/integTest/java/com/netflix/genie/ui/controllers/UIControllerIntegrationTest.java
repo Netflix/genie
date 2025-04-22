148d0a595aa10e7a7eaf151acce26af9e2475564
Iuliia Sobolevska <isobolevska@netflix.com>
2025-04-22 13:26:50 -0700

    migrate off deprecated @MockBean annotation

--- genie-ui/src/integTest/java/com/netflix/genie/ui/controllers/UIControllerIntegrationTest.java
+++ genie-ui/src/integTest/java/com/netflix/genie/ui/controllers/UIControllerIntegrationTest.java
@@ -24,7 +24,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -52,7 +52,7 @@ class UIControllerIntegrationTest {
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
+    @MockitoBean
     private GenieExceptionMapper genieExceptionMapper;
 
     /**
