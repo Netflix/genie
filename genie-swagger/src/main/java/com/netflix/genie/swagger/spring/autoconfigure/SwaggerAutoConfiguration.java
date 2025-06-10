06112e8680ce2fcee82ca908d1cdf3d32f045543
Iuliia Sobolevska <isobolevska@netflix.com>
2025-06-09 17:10:45 -0700

    update SpringDoc as version 1 is incompatible with Spring Boot 3

--- genie-swagger/src/main/java/com/netflix/genie/swagger/spring/autoconfigure/SwaggerAutoConfiguration.java
+++ genie-swagger/src/main/java/com/netflix/genie/swagger/spring/autoconfigure/SwaggerAutoConfiguration.java
@@ -21,7 +21,7 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
