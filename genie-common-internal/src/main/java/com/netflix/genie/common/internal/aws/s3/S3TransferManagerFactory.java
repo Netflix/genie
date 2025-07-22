1566e8fa5565b4a4567bd91578f2d03cded6c7b8
Binbing Hou <houbinbing.cs.hust@gmail.com>
2025-07-21 20:26:48 -0700

    Improve S3AsyncClient multipart upload for large files (#1250)
    * set S3AsyncClient with multipartEnabled=true
    
    * update the python image of genie demo to 3.10-slim
    
    * set chunk size to 256 MB for large files
    
    * set chunk size to 256 MB for large files
    
    * nit
    
    ---------
    
    Co-authored-by: Binbing Hou <bhou@netflix.com>

--- genie-common-internal/src/main/java/com/netflix/genie/common/internal/aws/s3/S3TransferManagerFactory.java
+++ genie-common-internal/src/main/java/com/netflix/genie/common/internal/aws/s3/S3TransferManagerFactory.java
@@ -23,7 +23,6 @@ import software.amazon.awssdk.services.s3.S3AsyncClient;
 import software.amazon.awssdk.services.s3.S3Uri;
 import software.amazon.awssdk.services.sts.auth.StsAssumeRoleCredentialsProvider;
 import software.amazon.awssdk.transfer.s3.S3TransferManager;
-
 import java.net.URI;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -36,6 +35,7 @@ import java.util.concurrent.ConcurrentHashMap;
 @Slf4j
 public class S3TransferManagerFactory {
 
+    private static final long MIN_PART_SIZE_IN_BYTES = 256 * 1024 * 1024L;
     private final S3ClientFactory s3ClientFactory;
     private final ConcurrentHashMap<S3ClientFactory.S3ClientKey, S3AsyncClient> asyncClientCache;
     private final ConcurrentHashMap<S3AsyncClient, S3TransferManager> transferManagerCache;
@@ -115,6 +115,8 @@ public class S3TransferManagerFactory {
         return S3AsyncClient.builder()
             .region(s3ClientKey.getRegion())
             .credentialsProvider(credentialsProvider)
+            .multipartEnabled(true)
+            .multipartConfiguration(configBuilder -> configBuilder.minimumPartSizeInBytes(MIN_PART_SIZE_IN_BYTES))
             .build();
     }
 
